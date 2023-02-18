ed34871e99aa73c612d01b17ea0298639ed7efcd
Victor Mustar <victor.mustar@gmail.com>
2023-02-18 14:13:00 +0100

    layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,140 +1,70 @@
-<div class="flex bg-black/95 text-gray-300/90">
-	<div class="w-64">
-		<button class="text-white p-4 border border-white">New chat</button>
+<div class="grid h-screen w-screen md:grid-cols-[256px,1fr] overflow-hidden text-smd">
+	<div class="max-md:hidden bg-gray-900/20 pt-12 px-6">
+		<button
+			class="border px-12 py-2.5 rounded-lg bg-gray-800/20 border border-gray-800/50 shadow w-full"
+			>New Chat</button
+		>
 	</div>
-	<div class="relative flex-1 overflow-y-auto">
-		<div class="mx-auto flex h-screen max-h-screen max-w-4xl flex-col gap-8 pt-12">
-			<div class="flex items-start justify-start gap-4 leading-relaxed">
-				<img
-					src="https://huggingface.co/avatars/2edb18bd0206c16b433841a47f53fa8e.svg"
-					class="mt-5 h-4 w-4 flex-none rounded-full shadow-lg shadow-white/40"
-				/>
-				<div
-					class="text-blue-00 group relative rounded-2xl bg-gradient-to-br from-gray-800/50 to-gray-800/20 px-5 pt-4 pb-5"
-				>
-					Lorem ipsum, dolor sit amet consectetur adipisicing eLorem ipsum, dolor sit amet
-					consectetur adipisicing eLorem ipsum, dolor sit amet consectetur adipisicing eLorem ipsum,
-					dolor sit amet consectetur adipisicing eLorem ipsum, dolor sit amet consectetur
-					adipisicing eLorem ipsum, dolor sit amet consectetur adipisicing eLorem ipsum, dolor sit
-					amet consectetur adipisicing elit. Dolorum maiores quo officiis?
-				</div>
-			</div>
-			<div class="mb-2 flex items-start justify-start gap-4 text-gray-300/70">
-				<div class="mt-4 h-4 w-4 flex-none rounded-full" />
-				<div class="rounded-2xl px-4 py-3.5">
-					Lorem ipsum, dolor sit amet consectetur adipisicing eLorem ipsum, dolor sit amet
-					consectetur adipisicing eLorem ipsum
-				</div>
-			</div>
-			<div class="flex items-start justify-start gap-4 leading-relaxed">
-				<img
-					src="https://huggingface.co/avatars/2edb18bd0206c16b433841a47f53fa8e.svg"
-					class="mt-5 h-3 w-3 flex-none rounded-full bg-white"
-				/>
-				<div
-					class="text-blue-00 rounded-2xl bg-gradient-to-br from-gray-800/50 to-gray-800/20 px-5 pt-4 pb-5"
-				>
-					<p>
+	<div class="overflow-y-auto">
+		<div class="max-w-4xl mx-auto px-5 pt-12 flex flex-col gap-8">
+			{#each Array(4) as _}
+				<div class="flex items-start justify-start gap-4 leading-relaxed">
+					<img
+						src="https://huggingface.co/avatars/2edb18bd0206c16b433841a47f53fa8e.svg"
+						class="mt-5 h-3.5 w-3.5 flex-none rounded-full shadow-lg shadow-white/40"
+					/>
+					<div
+						class="group relative rounded-2xl bg-gradient-to-br from-gray-800/50 to-gray-800/20 px-5 py-3.5"
+					>
 						Lorem ipsum, dolor sit amet consectetur adipisicing eLorem ipsum, dolor sit amet
-						consectetur adipisicing eLorem ipsum, dolor sit amet consectetur adipisicing eLorem
-						ipsum, dolor sit
-					</p>
-					<div class="mt-4 flex flex-wrap gap-4 first:mt-0">
-						<img
-							src="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/7a6fcbc9-3440-49b4-9c33-eaadc59a462d"
-							alt=""
-							class="max-h-[360px] rounded-xl"
-						/>
-						<img
-							src="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/11d91b58-4305-45de-b608-1142ddc118a7"
-							alt=""
-							class="max-h-[360px] rounded-xl"
-						/>
+						consectetur adipisicing iciis? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Rem, qui consequatur voluptas inventore recusandae animi voluptate consectetur aperiam,
+						sapiente aliqm possimus quasi accusamus, dolores nobis officiis vitae tenetur? Porro,
+						corporis.
 					</div>
 				</div>
-			</div>
-			<div class="flex items-start justify-start gap-4 leading-relaxed">
-				<img
-					src="https://huggingface.co/avatars/2edb18bd0206c16b433841a47f53fa8e.svg"
-					class="mt-5 h-3 w-3 flex-none rounded-full bg-white"
-				/>
-				<div
-					class="text-blue-00 rounded-2xl bg-gradient-to-br from-gray-800/50 to-gray-800/20 px-5 pt-4 pb-5"
-				>
-					<p>
+				<div class="mb-2 flex items-start justify-start gap-4 text-gray-300/80">
+					<div class="mt-4 h-3.5 w-3.5 flex-none rounded-full" />
+					<div class="rounded-2xl px-5 py-3.5">
 						Lorem ipsum, dolor sit amet consectetur adipisicing eLorem ipsum, dolor sit amet
-						consectetur adipisicing eLorem ipsum, dolor sit amet consectetur adipisicing eLorem
-						ipsum, dolor sit
-					</p>
-					<div class="mt-4 flex flex-wrap gap-4 first:mt-0">
-						<img
-							src="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/7a6fcbc9-3440-49b4-9c33-eaadc59a462d"
-							alt=""
-							class="max-h-[360px] rounded-xl"
-						/>
-						<img
-							src="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/11d91b58-4305-45de-b608-1142ddc118a7"
-							alt=""
-							class="max-h-[360px] rounded-xl"
-						/>
+						consectetur adipisicing eLorem ipsum
 					</div>
 				</div>
-			</div>
-			<div class="flex items-start justify-start gap-4 leading-relaxed">
-				<img
-					src="https://huggingface.co/avatars/2edb18bd0206c16b433841a47f53fa8e.svg"
-					class="mt-5 h-3 w-3 flex-none rounded-full bg-white"
-				/>
-				<div
-					class="text-blue-00 rounded-2xl bg-gradient-to-br from-gray-800/50 to-gray-800/20 px-5 pt-4 pb-5"
-				>
-					<p>
-						Lorem ipsum, dolor sit amet consectetur adipisicing eLorem ipsum, dolor sit amet
-						consectetur adipisicing eLorem ipsum, dolor sit amet consectetur adipisicing eLorem
-						ipsum, dolor sit
-					</p>
-					<div class="mt-4 flex flex-wrap gap-4 first:mt-0">
-						<img
-							src="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/7a6fcbc9-3440-49b4-9c33-eaadc59a462d"
-							alt=""
-							class="max-h-[360px] rounded-xl"
-						/>
-						<img
-							src="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/11d91b58-4305-45de-b608-1142ddc118a7"
-							alt=""
-							class="max-h-[360px] rounded-xl"
-						/>
-					</div>
-				</div>
-			</div>
-			<div class="flex items-start justify-start gap-4 text-gray-300">
-				<div class="mt-4 h-4 w-4 flex-none rounded-full" />
-				<div class="rounded-2xl px-4 py-3.5">
-					Lorem ipsum, dolor sit amet consectetur adipisicing eLorem ipsum, dolor sit amet
-					consectetur adipisicing eLorem ipsum
-				</div>
-			</div>
-			<div class="flex items-start justify-start gap-4 leading-relaxed">
-				<img
-					src="https://huggingface.co/avatars/2edb18bd0206c16b433841a47f53fa8e.svg"
-					class="mt-5 h-4 w-4 flex-none rounded-full shadow-lg shadow-white/40"
-				/>
-				<div
-					class="group relative rounded-2xl bg-gradient-to-br from-gray-800/50 to-gray-800/20 px-5 pt-4 pb-5 text-blue-900"
-				>
-					<div class="h-4 w-2 animate-pulse bg-white/20" />
-				</div>
-			</div>
+			{/each}
+			<div class="h-32" />
 		</div>
-		<div class="fixed inset-x-0 bottom-12 mx-auto max-w-4xl">
-			<div
-				class="w-full rounded-2xl border border-black bg-gradient-to-br from-gray-800/40 to-gray-800/50 px-4 py-3.5 shadow-2xl shadow-white/5 outline-none ring-gray-700 focus:ring-1"
-				role="textbox"
-				contenteditable
+	</div>
+	<div
+		class="flex items-center justify-center absolute left-[256px] right-0  px-5 bottom-0 h-32 bg-gradient-to-t from-black to-black/0"
+	>
+		<div
+			class="shadow-alternate relative flex items-center rounded-xl border border-gray-100 dark:bg-gray-950"
+		>
+			<svg
+				class="absolute left-3 text-gray-300 top-1/2 transform -translate-y-1/2 pointer-events-none"
+				xmlns="http://www.w3.org/2000/svg"
+				xmlns:xlink="http://www.w3.org/1999/xlink"
+				aria-hidden="true"
+				focusable="false"
+				role="img"
+				width="1em"
+				height="1em"
+				preserveAspectRatio="xMidYMid meet"
+				viewBox="0 0 32 32"
+				><path
+					d="M30 28.59L22.45 21A11 11 0 1 0 21 22.45L28.59 30zM5 14a9 9 0 1 1 9 9a9 9 0 0 1-9-9z"
+					fill="currentColor"
+				/></svg
 			>
-				Ask anything...
-			</div>
-			<!-- <textarea class="w-full rounded-2xl bg-slate-800/80 px-4 py-3.5 shadow-xl" rows="1" placeholder="Write anything..."></textarea> -->
+			<input
+				class="flex-1 border-none bg-transparent px-1 py-2 pr-3 pl-10 outline-none placeholder:text-gray-400"
+				placeholder="Search anything on the Hub"
+			/>
 		</div>
+		<!-- <input
+			type="text"
+			placeholder="Type anything..."
+			class="w-full rounded-2xl border border-black bg-black px-4 py-3.5 shadow-2xl shadow-white/5 outline-none ring-gray-700 focus:ring-1 max-w-4xl"
+		/> -->
 	</div>
 </div>
