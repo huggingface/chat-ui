a3ae6ee7790eb7d0e949c4a7bfddc6c9aa2bcbc1
Victor Mustar <victor.mustar@gmail.com>
2023-02-19 17:49:43 +0100

    chat intro

---
+++ src/lib/chat/ChatIntroduction.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let title: string = '';
+</script>
+
+<div class="grid grid-cols-3 gap-3 my-auto">
+	<div class="col-span-3 text-center mb-10">
+		<h1 class="text-3xl font-bold mb-1.5">{title}</h1>
+		<p class="text-lg text-gray-500">Lorem ipsum dolor sit.</p>
+	</div>
+	<div class="bg-gray-50 rounded-xl text-gray-500 p-4">Create</div>
+	<div class="bg-gray-50 rounded-xl text-gray-500 p-4">Discover</div>
+	<div class="bg-gray-50 rounded-xl text-gray-500 p-4">Overcome</div>
+	<div class="bg-gray-50 rounded-xl text-gray-500 p-4">Create</div>
+	<div class="bg-gray-50 rounded-xl text-gray-500 p-4">Discover</div>
+	<div class="bg-gray-50 rounded-xl text-gray-500 p-4">Overcome</div>
+</div>
