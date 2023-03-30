510e33dea113908efe1e910dc1fd74c3a2d7cfc9
Julien Chaumond <julien@huggingface.co>
2023-03-30 15:04:34 +0200

    some types + some API tweaks
    Co-Authored-By: OlivierDehaene <23298448+OlivierDehaene@users.noreply.github.com>

--- src/lib/Types.ts
+++ src/lib/Types.ts
@@ -8,3 +8,23 @@ export type Message =
 	content: string;
 };
 
+
+export interface Token {
+	id:      number;
+	text:    string;
+	logprob: number;
+	special: boolean;
+}
+
+
+export interface StreamResponse {
+	/**
+	 * Generated token
+	 */
+	token: Token;
+	/**
+	 * Complete generated text
+	 * Only available when the generation is finished
+	 */
+	generated_text?: string;
+}
