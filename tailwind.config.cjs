f77e15743ac17125da038af6d4110f679e4df07d
Adrien Denat <oi@adriendenat.com>
2023-04-12 18:20:20 +0200

    migrate to integrated Tailwind instead of CDN
    so we have autocompletion support in VSCode and other stuff

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			// fontFamily: {
+			// 	sans: ['"Inter"', ...defaultTheme.fontFamily.sans]
+			// },
+			fontSize: {
+				smd: '0.94rem'
+			}
+		}
+	},
+	plugins: [require('tailwind-scrollbar')({ nocompatible: true })]
+};
