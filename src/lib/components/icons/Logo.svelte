230bbf23fca7330cc1752c1021f38bca71c19661
Victor Mustar <victor.mustar@gmail.com>
2023-04-17 12:34:24 +0200

    intro design update

---
+++ src/lib/components/icons/Logo.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let classNames: string = '';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="1em"
+	height="1em"
+	class={classNames}
+	viewBox="0 0 24 25"
+>
+	<path
+		fill="currentColor"
+		d="M2.7 11.4c0-4.2 3.4-7.6 7.6-7.6h4a7 7 0 0 1 7 7v.6a7 7 0 0 1-7 7H5.6l-2.3 2.1c-.2.2-.5 0-.5-.2V11.4Z"
+	/>
+</svg>
