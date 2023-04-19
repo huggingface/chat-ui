abc354c24016407d719127971efc6af67361e11a
Adrien Denat <adrien.denat@gmail.com>
2023-04-19 15:47:59 +0200

    Chat snap scroll bottom (#10)
    * only snap scroll to bottom if user didn't scroll up
    
    * add scroll to bottom button when user scrolls up in the chat
    
    * remove unecessary export
    
    * rename scrollToBottom action to snapScrollToBottom + remove anim for now as it's stuttering

---
+++ src/lib/components/icons/Chevron.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let classNames: string = '';
+</script>
+
+<svg
+	width="15"
+	height="8"
+	viewBox="0 0 15 8"
+	class={classNames}
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		d="M1.67236 1L7.67236 7L13.6724 1"
+		stroke="currentColor"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
