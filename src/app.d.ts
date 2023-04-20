9d8e85840f3cc8ea35a78a659a43babed1ff4cd0
Victor Mustar <victor.mustar@gmail.com>
2023-04-20 17:26:15 +0200

    add icons lib

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,6 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference types="unplugin-icons/types/svelte" />
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
