4c70bcc71da30bc968f5d4a1c2415a9cd5252a9e
Julien Chaumond <julien@huggingface.co>
2023-04-20 19:31:55 +0200

    auto-formatting (#45)

--- src/lib/components/icons/IconCopy.svelte
+++ src/lib/components/icons/IconCopy.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	export let classNames = "";
+	export let classNames = '';
 </script>
 
 <svg
@@ -18,5 +18,9 @@
 		d="M28,10V28H10V10H28m0-2H10a2,2,0,0,0-2,2V28a2,2,0,0,0,2,2H28a2,2,0,0,0,2-2V10a2,2,0,0,0-2-2Z"
 		transform="translate(0)"
 	/>
-	<path d="M4,18H2V4A2,2,0,0,1,4,2H18V4H4Z" transform="translate(0)" /><rect fill="none" width="32" height="32" />
+	<path d="M4,18H2V4A2,2,0,0,1,4,2H18V4H4Z" transform="translate(0)" /><rect
+		fill="none"
+		width="32"
+		height="32"
+	/>
 </svg>
