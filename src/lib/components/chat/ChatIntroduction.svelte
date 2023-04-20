0631ae961ae5b80f17d237807fc37895e5372ea5
Eliott C <coyotte508@gmail.com>
2023-04-20 17:33:34 +0200

    ✨ Make chat intro buttons work (#38)

--- src/lib/components/chat/ChatIntroduction.svelte
+++ src/lib/components/chat/ChatIntroduction.svelte
@@ -2,6 +2,9 @@
 	import { PUBLIC_DISABLE_INTRO_TILES, PUBLIC_MODEL_NAME } from '$env/static/public';
 
 	import Logo from '$lib/components/icons/Logo.svelte';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher<{ message: string }>();
 </script>
 
 <div class="grid lg:grid-cols-3 gap-8 my-auto">
@@ -26,34 +29,56 @@
 				class="flex items-center gap-4 px-3 py-2 bg-gray-100 rounded-xl text-sm text-gray-600 dark:text-gray-300 dark:bg-gray-800"
 			>
 				<!-- need to change link to a public model before launch -->
-				<a href="https://huggingface.co/OpenAssistant/llama_30b_oasst_latcyr_1000" target="_blank"
-					>Model page</a
+				<a
+					href="https://huggingface.co/OpenAssistant/llama_30b_oasst_latcyr_1000"
+					target="_blank"
+					rel="noreferrer"
 				>
-				<a href="https://huggingface.co/datasets/OpenAssistant/oasst1" target="_blank"
-					>Dataset page</a
+					Model page
+				</a>
+				<a
+					href="https://huggingface.co/datasets/OpenAssistant/oasst1"
+					target="_blank"
+					rel="noreferrer"
 				>
-				<a href="https://open-assistant.io/" target="_blank" class="ml-auto"
-					>Open Assistant website</a
-				>
+					Dataset page
+				</a>
+				<a href="https://open-assistant.io/" target="_blank" class="ml-auto" rel="noreferrer">
+					Open Assistant website
+				</a>
 			</div>
 		</div>
 	</div>
-	{#if PUBLIC_DISABLE_INTRO_TILES}
+	{#if PUBLIC_DISABLE_INTRO_TILES !== 'true'}
 		<div class="lg:col-span-3 lg:mt-12">
 			<p class="mb-3 text-gray-600 dark:text-gray-300">Examples</p>
 			<div class="grid lg:grid-cols-3 gap-3 lg:gap-5">
 				<button
+					type="button"
 					class="text-gray-600 dark:text-gray-300 p-4 bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 hover:bg-gray-100 border dark:border-gray-800 rounded-xl"
-					>"Write an email from bullet list"</button
+					on:click={() =>
+						dispatch(
+							'message',
+							'Write an email from bullet list: \n\n- Buy milk\n- Buy eggs\n- Buy bread'
+						)}
 				>
+					"Write an email from bullet list"
+				</button>
 				<button
+					type="button"
 					class="text-gray-600 dark:text-gray-300 p-4 bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 hover:bg-gray-100 border dark:border-gray-800 rounded-xl"
-					>"Code a snake game"</button
+					on:click={() =>
+						dispatch('message', 'Code a snake game in python, the snake should be red')}
 				>
+					"Code a snake game"
+				</button>
 				<button
+					type="button"
 					class="text-gray-600 dark:text-gray-300 p-4 bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 hover:bg-gray-100 border dark:border-gray-800 rounded-xl"
-					>"Assist in a task"</button
+					on:click={() => dispatch('message', 'How do I make a lemon cheesecake?')}
 				>
+					"Assist in a task"
+				</button>
 			</div>
 		</div>
 	{/if}
