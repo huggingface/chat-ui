a32e428bc38f1992ab0a6d1f038b5a4bc2e3ac9b
coyotte508 <coyotte508@gmail.com>
2023-04-20 18:15:34 +0200

    💡

--- src/routes/r/[id]/+page.server.ts
+++ src/routes/r/[id]/+page.server.ts
@@ -3,7 +3,6 @@ import { collections } from '$lib/server/database';
 import { error } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ params }) => {
-	// todo: add validation on params.id
 	const conversation = await collections.sharedConversations.findOne({
 		_id: params.id
 	});
