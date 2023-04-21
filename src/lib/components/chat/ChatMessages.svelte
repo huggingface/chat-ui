3aa8136199968c091bdb37a14f36c8f53ac4ae64
Adrien Denat <adrien.denat@gmail.com>
2023-04-21 14:24:47 +0200

    add loading icon + pending state when assistant message is pending (#48)
    * add loading icon + pending state when assistant message is pending
    
    * remove dead code
    
    Co-authored-by: Eliott C. <coyotte508@gmail.com>
    
    * add loading to messages if a token takes a while to come
    
    * add dom utils
    
    ---------
    
    Co-authored-by: Eliott C. <coyotte508@gmail.com>

--- src/lib/components/chat/ChatMessages.svelte
+++ src/lib/components/chat/ChatMessages.svelte
@@ -6,17 +6,22 @@
 	import ChatMessage from './ChatMessage.svelte';
 
 	export let messages: Message[];
+	export let loading: boolean;
+	export let pending: boolean;
 
 	let chatContainer: HTMLElement;
 </script>
 
 <div class="overflow-y-auto h-full" use:snapScrollToBottom={messages} bind:this={chatContainer}>
 	<div class="max-w-3xl xl:max-w-4xl mx-auto px-5 pt-6 flex flex-col gap-8 h-full">
-		{#each messages as message}
-			<ChatMessage {message} />
+		{#each messages as message, i}
+			<ChatMessage loading={loading && i === messages.length - 1} {message} />
 		{:else}
 			<ChatIntroduction on:message />
 		{/each}
+		{#if pending}
+			<ChatMessage message={{ from: 'assistant', content: '' }} />
+		{/if}
 		<div class="h-32 flex-none" />
 	</div>
 	<ScrollToBottomBtn class="bottom-10 right-12" scrollNode={chatContainer} />
--- src/lib/components/chat/ChatWindow.svelte
+++ src/lib/components/chat/ChatWindow.svelte
@@ -8,7 +8,9 @@
 	import ChatInput from './ChatInput.svelte';
 
 	export let messages: Message[] = [];
-	export let disabled: boolean;
+	export let disabled: boolean = false;
+	export let loading: boolean = false;
+	export let pending: boolean = false;
 
 	let message: string;
 
@@ -21,28 +23,23 @@
 		<button>New Chat</button>
 		<button>+</button>
 	</nav>
-	<ChatMessages {messages} on:message />
+	<ChatMessages {loading} {pending} {messages} on:message />
 	<div
 		class="flex max-md:border-t dark:border-gray-800 items-center max-md:dark:bg-gray-900 max-md:bg-white bg-gradient-to-t from-white dark:from-gray-900 to-transparent justify-center absolute inset-x-0 max-w-3xl xl:max-w-4xl mx-auto px-5 bottom-0 py-4 md:py-8 w-full"
 	>
 		<form
 			on:submit|preventDefault={() => {
+				if (loading) return;
 				dispatch('message', message);
 				message = '';
 			}}
 			class="w-full relative flex items-center rounded-xl flex-1 max-w-4xl border bg-gray-100 focus-within:border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:focus-within:border-gray-500 transition-all"
 		>
 			<div class="w-full flex flex-1 border-none bg-transparent">
-				<ChatInput
-					placeholder="Ask anything"
-					bind:value={message}
-					{disabled}
-					autofocus
-					maxRows={10}
-				/>
+				<ChatInput placeholder="Ask anything" bind:value={message} autofocus maxRows={10} />
 				<button
 					class="p-1 px-[0.7rem] group self-end my-1 h-[2.4rem] rounded-lg hover:bg-gray-100 enabled:dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent disabled:opacity-60 dark:disabled:opacity-40 flex-shrink-0 transition-all mx-1"
-					disabled={!message || disabled}
+					disabled={!message || loading || disabled}
 					type="submit"
 				>
 					<CarbonSendAltFilled
