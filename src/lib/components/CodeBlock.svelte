10f14620b7b65511d28fb2227bc46429ceb20032
Adrien Denat <adrien.denat@gmail.com>
2023-04-21 14:25:49 +0200

    remove unneeded hack in main.css (#51)

--- src/lib/components/CodeBlock.svelte
+++ src/lib/components/CodeBlock.svelte
@@ -15,8 +15,10 @@
 	});
 </script>
 
-<div class="group code-block">
-	<pre><code class="language-{lang}">{@html highlightedCode || code}</code></pre>
+<div class="group relative rounded-lg my-4">
+	<pre class="overflow-auto px-5 py-3.5"><code class="language-{lang}"
+			>{@html highlightedCode || code}</code
+		></pre>
 	<CopyToClipBoardBtn
 		classNames="absolute top-2 right-2 invisible opacity-0 group-hover:visible group-hover:opacity-100"
 		value={code}
