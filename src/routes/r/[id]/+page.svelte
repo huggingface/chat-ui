9843f6f5fefbf3e7b151849a2fcd1efccc12e8fe
Eliott C <coyotte508@gmail.com>
2023-04-21 16:21:03 +0200

    ✨ Can chat in shared convo (#55)

--- src/routes/r/[id]/+page.svelte
+++ src/routes/r/[id]/+page.svelte
@@ -1,8 +1,44 @@
 <script lang="ts">
+	import { goto } from "$app/navigation";
+	import { base } from "$app/paths";
+	import { page } from "$app/stores";
 	import ChatWindow from "$lib/components/chat/ChatWindow.svelte";
+	import { pendingMessage } from "$lib/stores/pendingMessage";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
+
+	let loading = false;
+
+	async function createConversation(message: string) {
+		try {
+			loading = true;
+			const res = await fetch(`${base}/conversation`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					fromShare: $page.params.id,
+				}),
+			});
+
+			if (!res.ok) {
+				alert("Error while creating conversation: " + (await res.text()));
+				return;
+			}
+
+			const { conversationId } = await res.json();
+
+			// Ugly hack to use a store as temp storage, feel free to improve ^^
+			pendingMessage.set(message);
+
+			// invalidateAll to update list of conversations
+			await goto(`${base}/conversation/${conversationId}`, { invalidateAll: true });
+		} finally {
+			loading = false;
+		}
+	}
 </script>
 
-<ChatWindow disabled={true} messages={data.messages} />
+<ChatWindow on:message={(ev) => createConversation(ev.detail)} messages={data.messages} {loading} />
