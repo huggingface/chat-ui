c51b182b91d975f1299a16ecd62b390d29c5d793
Adrien Denat <oi@adriendenat.com>
2023-04-25 12:46:29 +0100

    fix typo

--- src/lib/components/chat/ChatWindow.svelte
+++ src/lib/components/chat/ChatWindow.svelte
@@ -26,7 +26,7 @@
 		class="flex flex-col pointer-events-none [&>*]:pointer-events-auto max-md:border-t dark:border-gray-800 items-center max-md:dark:bg-gray-900 max-md:bg-white bg-gradient-to-t from-white via-white/80 to-white/0 dark:from-gray-900 dark:via-gray-80 dark:to-gray-900/0 justify-center absolute inset-x-0 max-w-3xl xl:max-w-4xl mx-auto px-3.5 sm:px-5 bottom-0 py-4 md:py-8 w-full z-0"
 	>
 		<StopGeneratingBtn
-			visible={true}
+			visible={loading}
 			className="right-5 mr-[1px] md:mr-0 md:right-7 top-6 md:top-10 z-10"
 			on:click={() => dispatch("stop")}
 		/>
