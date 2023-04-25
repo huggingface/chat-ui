071476419227ddd815deedc83362c43ebf53507d
Adrien Denat <adrien.denat@gmail.com>
2023-04-25 13:00:36 +0200

    Styling improvements (#82)
    * add custom scrollbar everywhere
    
    * fix scroll to bottom button positioning + add hover
    
    * fix non-scrollable transparent area above input text
    
    * replace slate color by gray
    
    * add custom scrollbars for codeblock
    
    * fix chat messages overflowing on small viewports
    
    * fix codeblock scrollbar color on light mode + remove !important
    
    * fix send icon hover + make disabled buttons default cursor

--- src/lib/components/chat/ChatMessages.svelte
+++ src/lib/components/chat/ChatMessages.svelte
@@ -24,7 +24,11 @@
 	}
 </script>
 
-<div class="overflow-y-auto h-full" use:snapScrollToBottom={messages} bind:this={chatContainer}>
+<div
+	class="overflow-y-auto h-full scrollbar-custom mr-1"
+	use:snapScrollToBottom={messages}
+	bind:this={chatContainer}
+>
 	<div class="max-w-3xl xl:max-w-4xl mx-auto px-5 pt-6 flex flex-col gap-5 sm:gap-8 h-full">
 		{#each messages as message, i}
 			<ChatMessage loading={loading && i === messages.length - 1} {message} />
@@ -36,5 +40,8 @@
 		{/if}
 		<div class="h-32 flex-none" />
 	</div>
-	<ScrollToBottomBtn class="bottom-10 right-12" scrollNode={chatContainer} />
+	<ScrollToBottomBtn
+		class="max-md:hidden bottom-36 right-4 lg:right-10"
+		scrollNode={chatContainer}
+	/>
 </div>
