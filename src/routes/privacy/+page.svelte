97b65ef434a340e4c1f8d2da6020ed12301c89c4
Julien Chaumond <julien@huggingface.co>
2023-04-25 16:06:44 +0200

    `/privacy` page, rendered from markdown (#100)
    * /privacy from markdown
    
    * improve style...
    
    * ♻️ Simpler loading of PRIVACY.md
    
    * improve style
    
    * Better formatting
    
    * 💄 Better scrolling
    
    * 🔥 Remove file committed by mistake
    
    * Apply suggestions from code review
    
    Co-authored-by: Omar Sanseviero <osanseviero@gmail.com>
    
    ---------
    
    Co-authored-by: coyotte508 <coyotte508@gmail.com>
    Co-authored-by: Omar Sanseviero <osanseviero@gmail.com>

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { marked } from "marked";
+	import privacy from "../../../PRIVACY.md?raw";
+</script>
+
+<div class="my-6 overflow-auto">
+	<div class="mx-auto pt-12 prose">
+		<h1>Hello</h1>
+		{@html marked(privacy, { gfm: true })}
+	</div>
+</div>
