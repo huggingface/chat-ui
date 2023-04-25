c1ed8617e9d6fc9f753f1d3d8b9653d0230a0b5c
Victor Mustar <victor.mustar@gmail.com>
2023-04-25 17:33:12 +0200

    privacy page tweaks

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -3,8 +3,8 @@
 	import privacy from "../../../PRIVACY.md?raw";
 </script>
 
-<div class="overflow-auto">
-	<div class="mx-auto py-12 prose dark:prose-invert">
+<div class="p-6 overflow-auto">
+	<div class="mx-auto pt-6 md:pt-12 pb-24 px-4 prose dark:prose-invert">
 		{@html marked(privacy, { gfm: true })}
 	</div>
 </div>
