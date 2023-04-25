72dcf63d6b1d0aa4119dae0d0ee5df27dec06ab8
Victor Mustar <victor.mustar@gmail.com>
2023-04-25 13:22:35 +0200

    design

--- src/lib/components/chat/ChatWindow.svelte
+++ src/lib/components/chat/ChatWindow.svelte
@@ -7,6 +7,8 @@
 	import ChatMessages from "./ChatMessages.svelte";
 	import ChatInput from "./ChatInput.svelte";
 	import CarbonExport from "~icons/carbon/export";
+	import Toast from "$lib/components/Toast.svelte";
+
 	import { PUBLIC_MODEL_NAME } from "$env/static/public";
 
 	export let messages: Message[] = [];
@@ -20,6 +22,7 @@
 </script>
 
 <div class="relative min-h-0 min-w-0">
+	<Toast />
 	<ChatMessages {loading} {pending} {messages} on:message />
 	<div
 		class="flex flex-col pointer-events-none [&>*]:pointer-events-auto max-md:border-t dark:border-gray-800 items-center max-md:dark:bg-gray-900 max-md:bg-white bg-gradient-to-t from-white via-white/80 to-white/0 dark:from-gray-900 dark:via-gray-80 dark:to-gray-900/0 justify-center absolute inset-x-0 max-w-3xl xl:max-w-4xl mx-auto px-3.5 sm:px-5 bottom-0 py-4 md:py-8 w-full"
