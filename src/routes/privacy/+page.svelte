76e469a2a7999e335b58ad215df797b067dd4b56
coyotte508 <coyotte508@gmail.com>
2023-04-25 15:58:37 +0200

    💄 Better scrolling

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -3,6 +3,9 @@
 	import privacy from "../../../PRIVACY.md?raw";
 </script>
 
-<div class="my-6 mt-12 mx-auto overflow-auto prose">
-	{@html marked(privacy, { gfm: true })}
+<div class="my-6 overflow-auto">
+	<div class="mx-auto pt-12 prose">
+		<h1>Hello</h1>
+		{@html marked(privacy, { gfm: true })}
+	</div>
 </div>
