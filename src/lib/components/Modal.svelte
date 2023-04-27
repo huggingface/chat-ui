6cec4b18f52c41ac1f5846276207d7304008dd7b
Victor Mustar <victor.mustar@gmail.com>
2023-04-27 12:46:17 +0200

    alignement

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -7,7 +7,7 @@
 	transition:fade={{ easing: cubicOut, duration: 300 }}
 	class="fixed inset-0 z-40 flex items-center justify-center bg-black/80 p-8 backdrop-blur-sm dark:bg-black/50"
 >
-	<div class="max-w-sm overflow-hidden rounded-2xl bg-white shadow-2xl">
+	<div class="-mt-10 max-w-sm overflow-hidden rounded-2xl bg-white shadow-2xl md:-mt-20">
 		<slot />
 	</div>
 </div>
