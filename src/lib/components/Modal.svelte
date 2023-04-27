6b915cdf3337b7756b26cb34ebd72afc9f14cf20
Victor Mustar <victor.mustar@gmail.com>
2023-04-27 11:36:22 +0200

    add ethics modal

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,5 @@
+<div class="fixed inset-0 z-40 flex items-center justify-center bg-black/80 p-8 backdrop-blur-sm">
+	<div class="max-w-sm overflow-hidden rounded-2xl bg-white shadow-2xl">
+		<slot />
+	</div>
+</div>
