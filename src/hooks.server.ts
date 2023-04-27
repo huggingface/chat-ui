184689c4ee0fbb40d294f3c73c0fcc8a90070cb7
Adrien Denat <adrien.denat@gmail.com>
2023-04-27 21:05:12 +0200

    Fix gtm loading (#139)
    * try to get GA env to load
    
    * rollback to injection from the server hook
    
    * remove dead code

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,7 @@
 import { dev } from "$app/environment";
 import { COOKIE_NAME } from "$env/static/private";
 import type { Handle } from "@sveltejs/kit";
+import { PUBLIC_GOOGLE_ANALYTICS_ID } from "$env/static/public";
 import { addYears } from "date-fns";
 
 export const handle: Handle = async ({ event, resolve }) => {
@@ -18,7 +19,19 @@ export const handle: Handle = async ({ event, resolve }) => {
 		expires: addYears(new Date(), 1),
 	});
 
-	const response = await resolve(event);
+	let replaced = false;
+
+	const response = await resolve(event, {
+		transformPageChunk: (chunk) => {
+			// For some reason, Sveltekit doesn't let us load env variables from .env in the app.html template
+			if (replaced || !chunk.html.includes("%gaId%")) {
+				return chunk.html;
+			}
+			replaced = true;
+
+			return chunk.html.replace("%gaId%", PUBLIC_GOOGLE_ANALYTICS_ID);
+		},
+	});
 
 	return response;
 };
