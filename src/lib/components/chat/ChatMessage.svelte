3a6a2b3a313d8c629e02e4d1f338b9da2305bd46
Victor Mustar <victor.mustar@gmail.com>
2023-04-28 13:04:03 +0200

    button position

--- src/lib/components/chat/ChatMessage.svelte
+++ src/lib/components/chat/ChatMessage.svelte
@@ -101,7 +101,7 @@
 		</div>
 		{#if !loading && message.id}
 			<button
-				class="absolute right-0 top-3.5 cursor-pointer rounded-lg border border-gray-100 p-1 text-xs text-gray-400 group-hover:block hover:text-gray-500 dark:border-gray-800 dark:text-gray-400 dark:hover:text-gray-300 md:hidden"
+				class="absolute right-0 top-3.5 cursor-pointer rounded-lg border border-gray-100 p-1 text-xs text-gray-400 group-hover:block hover:text-gray-500 dark:border-gray-800 dark:text-gray-400 dark:hover:text-gray-300 md:hidden lg:-right-2"
 				title="Retry"
 				type="button"
 				on:click={() => dispatch("retry")}
