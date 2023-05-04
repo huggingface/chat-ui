e79c2b2b75259425b0bc7dff1400eeca20ca4c72
coyotte508 <coyotte508@gmail.com>
2023-05-04 11:56:36 +0200

    ♿️ No-JS ethics modal

---
+++ src/routes/settings/+page.server.ts
@@ -0,0 +1,41 @@
+import { base } from "$app/paths";
+import { collections } from "$lib/server/database.js";
+import { redirect } from "@sveltejs/kit";
+import { z } from "zod";
+
+export const actions = {
+	default: async function ({ request, locals }) {
+		const formData = await request.formData();
+
+		const { ethicsModalAccepted, ...settings } = z
+			.object({
+				shareConversationsWithModelAuthors: z.boolean({ coerce: true }).default(true),
+				ethicsModalAccepted: z.boolean({ coerce: true }).optional(),
+			})
+			.parse({
+				shareConversationsWithModelAuthors: formData.get("shareConversationsWithModelAuthors"),
+				ethicsModalAccepted: formData.get("ethicsModalAccepted"),
+			});
+
+		await collections.settings.updateOne(
+			{
+				sessionId: locals.sessionId,
+			},
+			{
+				$set: {
+					...settings,
+					...(ethicsModalAccepted && { ethicsModalAcceptedAt: new Date() }),
+					updatedAt: new Date(),
+				},
+				$setOnInsert: {
+					createdAt: new Date(),
+				},
+			},
+			{
+				upsert: true,
+			}
+		);
+
+		throw redirect(303, request.headers.get("referer") || base || "/");
+	},
+};
