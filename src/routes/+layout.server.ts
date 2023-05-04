4b3c3187951f026d82d0493c1c9180dcb509d1da
Adrien Denat <oi@adriendenat.com>
2023-05-04 18:21:39 +0100

    make sure we allow user to change model if clicking on a link with model in url param

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,13 +1,33 @@
+import { base } from "$app/paths";
+import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 import { collections } from "$lib/server/database";
 import type { Conversation } from "$lib/types/Conversation";
 import { UrlDependency } from "$lib/types/UrlDependency";
 import { defaultModel, models } from "$lib/server/models";
+import { z } from "zod";
 
-export const load: LayoutServerLoad = async ({ locals, depends, url }) => {
+export const load: LayoutServerLoad = async ({ locals, depends, url, request }) => {
 	const { conversations } = collections;
+	const urlModel = url.searchParams.get("model");
 
 	depends(UrlDependency.ConversationList);
+
+	if (urlModel) {
+		try {
+			z.enum([models[0].name, ...models.slice(1).map((m) => m.name)]).safeParse(urlModel);
+
+			await collections.settings.updateOne(
+				{ sessionId: locals.sessionId },
+				{ $set: { activeModel: urlModel } },
+				{ upsert: true }
+			);
+		} catch (err) {
+			console.error(err);
+		}
+
+		throw redirect(303, request.headers.get("referer") || base || "/");
+	}
 
 	const settings = await collections.settings.findOne({ sessionId: locals.sessionId });
 
