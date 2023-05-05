fe5e8018ddb949832828c01722aa02c6a5d7615b
Adrien Denat <adrien.denat@gmail.com>
2023-05-05 15:36:49 +0200

    Improve modal a11y (#177)
    * add Portal component so we can render modals at root for focus trap
    
    * make modals closeable with ESC + click outside + focus trap
    
    * remove unecessary check on inert prop
    
    Co-authored-by: Eliott C. <coyotte508@gmail.com>
    
    ---------
    
    Co-authored-by: Eliott C. <coyotte508@gmail.com>

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,15 +1,59 @@
 <script lang="ts">
+	import { createEventDispatcher, onDestroy, onMount } from "svelte";
 	import { cubicOut } from "svelte/easing";
 	import { fade } from "svelte/transition";
+	import Portal from "./Portal.svelte";
+	import { browser } from "$app/environment";
 
 	export let width = "max-w-sm";
+
+	let backdropEl: HTMLDivElement;
+	let modalEl: HTMLDivElement;
+
+	const dispatch = createEventDispatcher<{ close: void }>();
+
+	function handleKeydown(event: KeyboardEvent) {
+		// close on ESC
+		if (event.key === "Escape") {
+			event.preventDefault();
+			dispatch("close");
+		}
+	}
+
+	function handleBackdropClick(event: MouseEvent) {
+		if (event.target === backdropEl) {
+			dispatch("close");
+		}
+	}
+
+	onMount(() => {
+		document.getElementById("app")?.setAttribute("inert", "true");
+		modalEl.focus();
+	});
+
+	onDestroy(() => {
+		if (!browser) return;
+		document.getElementById("app")?.removeAttribute("inert");
+	});
 </script>
 
-<div
-	transition:fade={{ easing: cubicOut, duration: 300 }}
-	class="fixed inset-0 z-40 flex items-center justify-center bg-black/80 p-8 backdrop-blur-sm dark:bg-black/50"
->
-	<div class="-mt-10 overflow-hidden rounded-2xl bg-white shadow-2xl md:-mt-20 {width}">
-		<slot />
+<Portal>
+	<div
+		role="presentation"
+		tabindex="-1"
+		bind:this={backdropEl}
+		on:click={handleBackdropClick}
+		transition:fade={{ easing: cubicOut, duration: 300 }}
+		class="fixed inset-0 z-40 flex items-center justify-center bg-black/80 p-8 backdrop-blur-sm dark:bg-black/50"
+	>
+		<div
+			role="dialog"
+			tabindex="-1"
+			bind:this={modalEl}
+			on:keydown={handleKeydown}
+			class="-mt-10 overflow-hidden rounded-2xl bg-white shadow-2xl outline-none md:-mt-20 {width}"
+		>
+			<slot />
+		</div>
 	</div>
-</div>
+</Portal>
