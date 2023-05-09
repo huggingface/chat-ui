cb644ddfe1e5eabeadb6f90473c5d32daca2977e
Adrien Denat <oi@adriendenat.com>
2023-05-09 19:38:15 +0100

    add better disabled state on chat input

--- src/lib/components/chat/ChatWindow.svelte
+++ src/lib/components/chat/ChatWindow.svelte
@@ -18,7 +18,7 @@
 	export let models: Model[] | undefined = undefined;
 	export let settings: LayoutData["settings"];
 
-	const isReadOnly = currentModel.disabled;
+	$: isReadOnly = currentModel.disabled;
 
 	let message: string;
 
@@ -59,7 +59,8 @@
 		/>
 		<form
 			on:submit|preventDefault={handleSubmit}
-			class="relative flex w-full max-w-4xl flex-1 items-center rounded-xl border bg-gray-100 focus-within:border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:focus-within:border-gray-500 "
+			class="relative flex w-full max-w-4xl flex-1 items-center rounded-xl border bg-gray-100 focus-within:border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:focus-within:border-gray-500 
+			{isReadOnly ? 'opacity-30' : ''}"
 		>
 			<div class="flex w-full flex-1 border-none bg-transparent">
 				<ChatInput
