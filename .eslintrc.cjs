5934bf6f0fbf1b9e6b710d9b1c423a89cac1e309
Adrien Denat <oi@adriendenat.com>
2023-05-09 09:48:05 +0100

    migrate to eslint-plugin-svelte from deprecated plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,27 @@
 module.exports = {
 	root: true,
 	parser: "@typescript-eslint/parser",
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:svelte/recommended",
+		"prettier",
+	],
 	plugins: ["svelte3", "@typescript-eslint"],
 	ignorePatterns: ["*.cjs"],
-	overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
-	settings: {
-		"svelte3/typescript": () => require("typescript"),
-	},
+	overrides: [
+		{
+			files: ["*.svelte"],
+			parser: "svelte-eslint-parser",
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+			},
+		},
+	],
 	parserOptions: {
 		sourceType: "module",
 		ecmaVersion: 2020,
+		extraFileExtensions: [".svelte"],
 	},
 	rules: {
 		"no-shadow": ["error"],
