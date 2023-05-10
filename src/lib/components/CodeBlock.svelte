3e7552d215809ce4296c452add59778215f40681
Eliott C <coyotte508@gmail.com>
2023-05-10 14:28:59 +0200

    🐛 Fix code formatting (#192)

--- src/lib/components/CodeBlock.svelte
+++ src/lib/components/CodeBlock.svelte
@@ -16,13 +16,11 @@
 </script>
 
 <div class="group relative my-4 rounded-lg">
+	<!-- eslint-disable svelte/no-at-html-tags -->
 	<pre
-		class="scrollbar-custom overflow-auto px-5 scrollbar-thumb-gray-500 hover:scrollbar-thumb-gray-400 dark:scrollbar-thumb-white/10 dark:hover:scrollbar-thumb-white/20">
-		<code class="language-{lang}">
-			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-			{@html highlightedCode || code.replaceAll("<", "&lt;")}
-		</code>
-	</pre>
+		class="scrollbar-custom overflow-auto px-5 scrollbar-thumb-gray-500 hover:scrollbar-thumb-gray-400 dark:scrollbar-thumb-white/10 dark:hover:scrollbar-thumb-white/20"><code
+			class="language-{lang}">{@html highlightedCode || code.replaceAll("<", "&lt;")}</code
+		></pre>
 	<CopyToClipBoardBtn
 		classNames="absolute top-2 right-2 invisible opacity-0 group-hover:visible group-hover:opacity-100"
 		value={code}
