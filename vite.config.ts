669cee3045b577a16dc2ef91ba79a06f3920126b
Saad Salman <132683924+ss9901@users.noreply.github.com>
2023-05-11 20:06:49 +0500

    Update vite.config.ts
    Now i just made a few changes, but its is going to make it easier to understand as a new user....

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import Icons from "unplugin-icons/vite";
 
+const { sveltekit: svelteKitPlugin, icons: iconsPlugin } = {
+  sveltekit,
+  icons: Icons({ compiler: "svelte" })
+};
+
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		Icons({
-			compiler: "svelte",
-		}),
-	],
+	plugins: [ svelteKitPlugin(), iconsPlugin ],
 });
