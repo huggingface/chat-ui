21120916d39ee9df3e846a9dbf1a5c59c0d8671a
Fred Guth <fredguth@fredguth.com>
2023-05-12 16:53:59 -0300

    add Brazilian Portuguese option

--- inlang.config.js
+++ inlang.config.js
@@ -1,14 +1,16 @@
-
 /**
  * @type { import("@inlang/core/config").DefineConfig }
  */
 export async function defineConfig(env) {
 	const { default: jsonPlugin } = await env.$import(
 		"https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js"
-	)
+	);
 	const { default: sdkPlugin } = await env.$import(
 		"https://cdn.jsdelivr.net/npm/@inlang/sdk-js-plugin@0.4.4/dist/index.js"
-	)
+	);
+	const { default: ideExtensionPlugin } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/ide-extension-plugin@latest/dist/index.js"
+	);
 
 	return {
 		referenceLanguage: "en",
@@ -16,11 +18,13 @@ export async function defineConfig(env) {
 			jsonPlugin({
 				pathPattern: "./languages/{language}.json",
 			}),
+
 			sdkPlugin({
 				languageNegotiation: {
-					strategies: [{ type: "localStorage" }]
-				}
+					strategies: [{ type: "localStorage" }],
+				},
 			}),
+			ideExtensionPlugin(),
 		],
-	}
+	};
 }
