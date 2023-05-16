f379516d9b93749017aca8e99ba0b79380adbb20
Adrien Denat <oi@adriendenat.com>
2023-05-16 11:05:33 +0100

    revert unnecessary change

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
