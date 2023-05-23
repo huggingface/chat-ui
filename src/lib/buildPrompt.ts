e1bc7eaa11e28c3208057c5438fe071f4610fbe1
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-05-23 08:54:47 +0200

    begin work on fetching webpage content

--- src/lib/buildPrompt.ts
+++ src/lib/buildPrompt.ts
@@ -1,18 +1,12 @@
 import type { BackendModel } from "./server/models";
 import { getQueryFromPrompt, searchWeb } from "./server/searchWeb";
 import type { Message } from "./types/Message";
-
+import { JSDOM } from 'jsdom';
 /**
  * Convert [{user: "assistant", content: "hi"}, {user: "user", content: "hello"}] to:
  *
  * <|assistant|>hi<|endoftext|><|prompter|>hello<|endoftext|><|assistant|>
  */
-
-interface SearchResult {
-	title: string;
-	snippet: string;
-	link: string;
-}
 
 export async function buildPrompt(
 	messages: Pick<Message, "from" | "content">[],
@@ -37,17 +31,29 @@ export async function buildPrompt(
 	let webPrompt = "";
 
 	if (webSearch) {
-		const query = await getQueryFromPrompt(messages);
-		console.log(query);
+		const query = await getQueryFromPrompt(messages, model);
+		console.log("query: ", query)
 		const results = await searchWeb(query);
 
+		// fetch the top result, summarize it and pass it in the context
+		if (results.organic_results) {
+			const topUrl = results.organic_results[0].link;
+			console.log("top result :", topUrl)
+			const htmlString = await fetch(topUrl)
+									.then((response)=>response.text())
+									.catch((err)=>console.log(err));
+
+			const dom = new JSDOM(htmlString??'');
+			const body = dom.window.document.querySelector('body');
+
+			console.log(body?.textContent?.replace((/  |\r\n|\n|\r/gm),""))
+		}
+		
 		webPrompt = "<|context|>" + (
 		  results.organic_results ?
 		  results.organic_results.map(element => `- ${element.snippet}`).join("\n") 
 		  : "No results found."
 		) + model.messageEndToken;
-
-		webPrompt += model.messageEndToken;
 	}
 
 	const finalPrompt =
