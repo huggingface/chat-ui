149f85a0b1d70698ad62984a91d4f127cc9a1aa6
Eliott C <coyotte508@gmail.com>
2023-05-24 09:34:29 +0200

    🥅 Display OIDC error properly (#261)

--- src/routes/login/callback/+page.server.ts
+++ src/routes/login/callback/+page.server.ts
@@ -4,16 +4,16 @@ import { z } from "zod";
 import { base } from "$app/paths";
 import { updateUser } from "./updateUser";
 
-export async function GET({ url, locals, cookies }) {
-	const { error: errorName } = z
+export async function load({ url, locals, cookies }) {
+	const { error: errorName, error_description: errorDescription } = z
 		.object({
 			error: z.string().optional(),
+			error_description: z.string().optional(),
 		})
 		.parse(Object.fromEntries(url.searchParams.entries()));
 
 	if (errorName) {
-		// TODO: Display denied error on the UI
-		throw redirect(302, `${base}/`);
+		throw error(400, errorName + (errorDescription ? ": " + errorDescription : ""));
 	}
 
 	const { code, state } = z
