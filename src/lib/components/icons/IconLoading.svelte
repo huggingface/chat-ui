8d75354c7b511f87bc2118163b57415d40c3b38d
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-06-02 17:39:31 +0200

    linter

--- src/lib/components/icons/IconLoading.svelte
+++ src/lib/components/icons/IconLoading.svelte
@@ -1,4 +1,8 @@
-<div class="inline-flex h-8 flex-none items-center gap-1">
+<script lang="ts">
+	export let classNames = "";
+</script>
+
+<div class={"inline-flex h-8 flex-none items-center gap-1 " + classNames}>
 	<div
 		class="h-1 w-1 animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
 		style="animation-delay: 0.25s;"
