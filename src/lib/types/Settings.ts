56e3cbf4af6da6b6a4cac262b39e4292c308e0d0
Ikko Eltociear Ashimine <eltociear@gmail.com>
2023-06-09 00:17:28 +0900

    Fix typo in Settings.ts
    explictly -> explicitly

--- src/lib/types/Settings.ts
+++ src/lib/types/Settings.ts
@@ -7,7 +7,7 @@ export interface Settings extends Timestamps {
 	sessionId?: string;
 
 	/**
-	 * Note: Only conversations with this settings explictly set to true should be shared.
+	 * Note: Only conversations with this settings explicitly set to true should be shared.
 	 *
 	 * This setting is explicitly set to true when users accept the ethics modal.
 	 * */
