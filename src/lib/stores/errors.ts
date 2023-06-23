001dd1bd8c4b67b152a3ada6051076d328c3387b
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-06-23 10:33:05 +0200

    very basic rate limiter (#320)

--- src/lib/stores/errors.ts
+++ src/lib/stores/errors.ts
@@ -3,6 +3,7 @@ import { writable } from "svelte/store";
 export const ERROR_MESSAGES = {
 	default: "Oops, something went wrong.",
 	authOnly: "You have to be logged in.",
+	rateLimited: "You are sending too many messages. Try again later.",
 };
 
 export const error = writable<string | null>(null);
