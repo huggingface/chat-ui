fb55900746a60388e97d18064818dc84818b3427
Victor Mustar <victor.mustar@gmail.com>
2023-07-06 13:52:42 +0200

    loader dots fix

--- src/lib/components/icons/IconLoading.svelte
+++ src/lib/components/icons/IconLoading.svelte
@@ -4,15 +4,15 @@
 
 <div class={"inline-flex h-8 flex-none items-center gap-1 " + classNames}>
 	<div
-		class="h-1 w-1 animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
+		class="h-1 w-1 flex-none animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
 		style="animation-delay: 0.25s;"
 	/>
 	<div
-		class="h-1 w-1 animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
+		class="h-1 w-1 flex-none animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
 		style="animation-delay: 0.5s;"
 	/>
 	<div
-		class="h-1 w-1 animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
+		class="h-1 w-1 flex-none animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
 		style="animation-delay: 0.75s;"
 	/>
 </div>
