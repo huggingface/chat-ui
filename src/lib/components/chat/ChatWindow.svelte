92a482a50e304c9c5ea990b28c84ac1b3bae7cd1
Albert Iribarne <iribarne@dribia.com>
2023-08-03 11:53:05 +0200

    Preserve compatibility with optional modelUrl config
    Use modelUrl if informed, else use the previous behavior.

--- src/lib/components/chat/ChatWindow.svelte
+++ src/lib/components/chat/ChatWindow.svelte
@@ -124,7 +124,7 @@
 		<div class="mt-2 flex justify-between self-stretch px-1 text-xs text-gray-400/90 max-sm:gap-2">
 			<p>
 				Model: <a
-					href="{currentModel.modelUrl}"
+					href={currentModel.modelUrl || "https://huggingface.co/" + currentModel.name}
 					target="_blank"
 					rel="noreferrer"
 					class="hover:underline">{currentModel.displayName}</a
