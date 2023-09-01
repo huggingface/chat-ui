fd8f654be3835e835651ad66f3b6e2e95b4cba5b
Mishig Davaadorj <dmishig@gmail.com>
2023-09-01 12:17:40 +0100

    update generate google query parser

--- src/lib/server/websearch/generateQuery.ts
+++ src/lib/server/websearch/generateQuery.ts
@@ -5,8 +5,9 @@ import { defaultModel } from "../models";
 export async function generateQuery(messages: Message[]) {
 	const promptSearchQuery = defaultModel.webSearchQueryPromptRender({ messages });
 	const searchQuery = await generateFromDefaultEndpoint(promptSearchQuery).then((query) => {
-		const arr = query.split(/\r?\n/);
-		return arr[0].length > 0 ? arr[0] : arr[1];
+		const regexLastPhrase = /("|'|`)((?:(?!\1).)+)\1$/;
+		const match = query.match(regexLastPhrase);
+		return match ? match[2] : promptSearchQuery;
 	});
 
 	return searchQuery;
