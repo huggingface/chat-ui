f55f12fb480d2c6f961f34ace53ef149d765f391
Mishig Davaadorj <dmishig@gmail.com>
2023-09-13 10:35:17 +0200

    comment for search query

--- src/lib/server/websearch/generateQuery.ts
+++ src/lib/server/websearch/generateQuery.ts
@@ -11,6 +11,12 @@ export async function generateQuery(messages: Message[]) {
 		currentDate,
 	});
 	const searchQuery = await generateFromDefaultEndpoint(promptSearchQuery).then((query) => {
+		// example of generating google query:
+		// user: tell me what happened yesterday
+		// LLM: google query is "news september 12, 2023"
+		// the regex below will try to capture the last "phrase" (i.e. words between quotes or double quotes or ticks)
+		// in this case, it is "news september 12, 2023"
+		// if there is no "phrase", we will just use the user query, which was "tell me what happened yesterday"
 		const regexLastPhrase = /("|'|`)((?:(?!\1).)+)\1$/;
 		const match = query.match(regexLastPhrase);
 		return match ? match[2] : lastMessage.content;
