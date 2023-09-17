271ba1eadac690a7716ff6495ea913a52ac8f53a
Julien Blanchon <julien@blanchon.cc>
2023-09-17 22:42:19 +0200

    Add openaiEndpoint model

--- src/lib/server/models.ts
+++ src/lib/server/models.ts
@@ -13,6 +13,16 @@ const sagemakerEndpoint = z.object({
 	sessionToken: z.string().optional(),
 });
 
+const openaiEndpoint = z.object({
+	host: z.literal("openai"),
+	url: z.string().url().default("https://api.openai.com/v1/chat/completions"),
+	apiKey: z.string().startsWith("sk-"),
+	model: z.string().min(1).default("gpt-3.5-turbo"),
+	temperature: z.number().min(0).max(1).default(0.9),
+	max_tokens: z.number().int().positive().optional(),
+});
+
+
 const tgiEndpoint = z.object({
 	host: z.union([z.literal("tgi"), z.undefined()]),
 	url: z.string().url(),
@@ -24,7 +34,11 @@ const commonEndpoint = z.object({
 });
 
 const endpoint = z.lazy(() =>
-	z.union([sagemakerEndpoint.merge(commonEndpoint), tgiEndpoint.merge(commonEndpoint)])
+	z.union([
+		sagemakerEndpoint.merge(commonEndpoint),
+		tgiEndpoint.merge(commonEndpoint),
+		openaiEndpoint.merge(commonEndpoint),
+	])
 );
 
 const combinedEndpoint = endpoint.transform((data) => {
@@ -32,6 +46,8 @@ const combinedEndpoint = endpoint.transform((data) => {
 		return tgiEndpoint.merge(commonEndpoint).parse(data);
 	} else if (data.host === "sagemaker") {
 		return sagemakerEndpoint.merge(commonEndpoint).parse(data);
+	} else if (data.host === "openai") {
+		return openaiEndpoint.merge(commonEndpoint).parse(data);
 	} else {
 		throw new Error(`Invalid host: ${data.host}`);
 	}
@@ -61,20 +77,20 @@ const modelsRaw = z
 				.string()
 				.default(
 					"{{preprompt}}" +
-						"{{#each messages}}" +
-						"{{#ifUser}}{{@root.userMessageToken}}{{content}}{{@root.userMessageEndToken}}{{/ifUser}}" +
-						"{{#ifAssistant}}{{@root.assistantMessageToken}}{{content}}{{@root.assistantMessageEndToken}}{{/ifAssistant}}" +
-						"{{/each}}" +
-						"{{assistantMessageToken}}"
+					"{{#each messages}}" +
+					"{{#ifUser}}{{@root.userMessageToken}}{{content}}{{@root.userMessageEndToken}}{{/ifUser}}" +
+					"{{#ifAssistant}}{{@root.assistantMessageToken}}{{content}}{{@root.assistantMessageEndToken}}{{/ifAssistant}}" +
+					"{{/each}}" +
+					"{{assistantMessageToken}}"
 				),
 			webSearchQueryPromptTemplate: z
 				.string()
 				.default(
 					"{{userMessageToken}}" +
-						'My question is: "{{message.content}}". ' +
-						"Based on the conversation history (my previous questions are: {{previousMessages}}), give me an appropriate query to answer my question for google search. You should not say more than query. You should not say any words except the query. For the context, today is {{currentDate}}" +
-						"{{userMessageEndToken}}" +
-						"{{assistantMessageToken}}"
+					'My question is: "{{message.content}}". ' +
+					"Based on the conversation history (my previous questions are: {{previousMessages}}), give me an appropriate query to answer my question for google search. You should not say more than query. You should not say any words except the query. For the context, today is {{currentDate}}" +
+					"{{userMessageEndToken}}" +
+					"{{assistantMessageToken}}"
 				),
 			promptExamples: z
 				.array(
@@ -117,15 +133,15 @@ export const models = await Promise.all(
 // Models that have been deprecated
 export const oldModels = OLD_MODELS
 	? z
-			.array(
-				z.object({
-					id: z.string().optional(),
-					name: z.string().min(1),
-					displayName: z.string().min(1).optional(),
-				})
-			)
-			.parse(JSON.parse(OLD_MODELS))
-			.map((m) => ({ ...m, id: m.id || m.name, displayName: m.displayName || m.name }))
+		.array(
+			z.object({
+				id: z.string().optional(),
+				name: z.string().min(1),
+				displayName: z.string().min(1).optional(),
+			})
+		)
+		.parse(JSON.parse(OLD_MODELS))
+		.map((m) => ({ ...m, id: m.id || m.name, displayName: m.displayName || m.name }))
 	: [];
 
 export type BackendModel = Optional<(typeof models)[0], "preprompt">;
