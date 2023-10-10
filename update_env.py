853516714d610f2349e33756428a695f7943b497
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-10-10 12:07:32 +0200

    black

--- update_env.py
+++ update_env.py
@@ -1,22 +1,17 @@
 import os
-from huggingface_hub import HfApi 
+from huggingface_hub import HfApi
 
 
-SECRET_CONFIG = os.environ['SECRET_CONFIG']
-HF_TOKEN = os.environ['HF_TOKEN']
+SECRET_CONFIG = os.environ["SECRET_CONFIG"]
+HF_TOKEN = os.environ["HF_TOKEN"]
 
 # Read the content of the file .env.template
-with open('.env.template', 'r') as f:
+with open(".env.template", "r") as f:
     PUBLIC_CONFIG = f.read()
 
 # Prepend the content of the env variable SECRET_CONFIG
-full_config = f'{PUBLIC_CONFIG}\n{SECRET_CONFIG}'
+full_config = f"{PUBLIC_CONFIG}\n{SECRET_CONFIG}"
 
 api = HfApi()
 
-api.add_space_secret(
-    "huggingchat/chat-ui",
-    "DOTENV_LOCAL",
-    full_config,
-    token=HF_TOKEN
-)
+api.add_space_secret("huggingchat/chat-ui", "DOTENV_LOCAL", full_config, token=HF_TOKEN)
