abc72dce958e5afd753551dd29c1b6618583899f
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-10-11 13:44:54 +0200

    summary with examples in chat format

--- src/lib/server/summarize.ts
+++ src/lib/server/summarize.ts
@@ -1,25 +1,21 @@
-import { buildPrompt } from "$lib/buildPrompt";
 import { generateFromDefaultEndpoint } from "$lib/server/generateFromDefaultEndpoint";
-import { defaultModel } from "$lib/server/models";
+import { smallModel } from "$lib/server/models";
+import type { Message } from "$lib/types/Message";
 
 export async function summarize(prompt: string) {
-	const userPrompt = `Please summarize the following message: \n` + prompt;
+	const messages: Array<Omit<Message, "id">> = [
+		{ from: "user", content: "Who is the president of France ?" },
+		{ from: "assistant", content: "🇫🇷 President of France request" },
+		{ from: "user", content: "What are the latest news ?" },
+		{ from: "assistant", content: "📰 Latest news" },
+		{ from: "user", content: "Can you debug this python code?" },
+		{ from: "assistant", content: "🐍 Python code debugging request" },
+		{ from: "user", content: prompt },
+	];
 
-	const summaryPrompt = await buildPrompt({
-		messages: [{ from: "user", content: userPrompt }],
-		preprompt: `
-You are a summarization AI. Your task is to summarize user requests, in a single sentence of less than 5 words. Do not try to answer questions, just summarize the user's request. Start your answer with an emoji relevant to the summary."
-
-Example: "Who is the president of France ?"
-Summary: "🇫🇷 President of France request"
-
-Example: "What are the latest news ?"
-Summary: "📰 Latest news"
-
-Example: "Can you debug this python code?"
-Summary: "🐍 Python code debugging request"
-`,
-		model: defaultModel,
+	const summaryPrompt = smallModel.chatPromptRender({
+		messages,
+		preprompt: `You are a summarization AI. Your task is to summarize user requests, in a single sentence of less than 5 words. Do not try to answer questions, just summarize the user's request. Start your answer with an emoji relevant to the summary.`,
 	});
 
 	const generated_text = await generateFromDefaultEndpoint(summaryPrompt).catch((e) => {
