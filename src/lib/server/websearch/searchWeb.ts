0bfb4dc3fcf0c77ebd4713c74ab6e155d0bcaf51
mrbean <sam@you.com>
2023-10-16 17:15:59 -0400

    add you.com integration

--- src/lib/server/websearch/searchWeb.ts
+++ src/lib/server/websearch/searchWeb.ts
@@ -1,17 +1,20 @@
-import { SERPAPI_KEY, SERPER_API_KEY } from "$env/static/private";
+import { SERPAPI_KEY, SERPER_API_KEY, YDC_API_KEY } from "$env/static/private";
 
 import { getJson } from "serpapi";
 import type { GoogleParameters } from "serpapi";
 
 // Show result as JSON
 export async function searchWeb(query: string) {
+	if (YDC_API_KEY) {
+		return await searchWebYouApi(query);
+	}
 	if (SERPER_API_KEY) {
 		return await searchWebSerper(query);
 	}
 	if (SERPAPI_KEY) {
 		return await searchWebSerpApi(query);
 	}
-	throw new Error("No Serper.dev or SerpAPI key found");
+	throw new Error("No You.com or Serper.dev or SerpAPI key found");
 }
 
 export async function searchWebSerper(query: string) {
@@ -58,4 +61,29 @@ export async function searchWebSerpApi(query: string) {
 	const response = await getJson("google", params);
 
 	return response;
+}
+
+export async function searchWebYouApi(query: string) {
+	const response = await fetch(`https://api.ydc-index.io/search?query=${query}`, {
+		method: "GET",
+		headers: {
+			"X-API-Key": YDC_API_KEY,
+			"Content-type": "application/json; charset=UTF-8",
+		},
+	});
+
+	/* eslint-disable @typescript-eslint/no-explicit-any */
+	const data = (await response.json()) as Record<string, any>;
+
+	if (!response.ok) {
+		throw new Error(
+			data["message"] ??
+				`You.com API returned error code ${response.status} - ${response.statusText}`
+		);
+	}
+
+
+	return {
+		organic_results: data.hits.map((hit) => hit.snippets.join("\n")) ?? [],
+	};
 }
