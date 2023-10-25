39936fd167cd1a6711a747e22ebd2cec50342f20
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-10-25 10:48:08 +0200

    Add a more visible retry button on failure to generate (#530)
    * Add a more visible retry button on failure to generate
    
    * clean up props
    
    * lint
    
    * simplify
    
    * color
    
    * fix linting
    
    ---------
    
    Co-authored-by: Victor Mustar <victor.mustar@gmail.com>

---
+++ src/lib/components/RetryBtn.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import CarbonRotate360 from "~icons/carbon/rotate-360";
+
+	export let classNames = "";
+</script>
+
+<button
+	type="button"
+	on:click
+	class="btn flex h-8 rounded-lg border bg-white px-3 py-1 text-gray-500 shadow-sm transition-all hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600 {classNames}"
+>
+	<CarbonRotate360 class="mr-2 text-xs " /> Retry
+</button>
