9f2789413e49a77ba46fb0ac1d139bf933e6a279
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-10-27 10:53:00 +0200

    fix lint

--- src/lib/components/DisclaimerModal.svelte
+++ src/lib/components/DisclaimerModal.svelte
@@ -12,7 +12,7 @@
 
 <Modal>
 	<div
-		class="from-primary-500/40 via-primary-500/10 to-primary-500/0 flex w-full flex-col items-center gap-6 bg-gradient-to-b px-5 pb-8 pt-9 text-center sm:px-6"
+		class="flex w-full flex-col items-center gap-6 bg-gradient-to-b from-primary-500/40 via-primary-500/10 to-primary-500/0 px-5 pb-8 pt-9 text-center sm:px-6"
 	>
 		<h2 class="flex items-center text-2xl font-semibold text-gray-800">
 			<Logo classNames="mr-1" />
