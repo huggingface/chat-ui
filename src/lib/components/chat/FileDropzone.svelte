04f241e1ec7d7b599464d35f3593c24f09f89cb8
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-10-30 10:48:36 +0100

    better error handling & max one image at a time

--- src/lib/components/chat/FileDropzone.svelte
+++ src/lib/components/chat/FileDropzone.svelte
@@ -16,35 +16,37 @@
 			if (files.length > 0) {
 				files = [];
 			}
-			for (let i = 0; i < event.dataTransfer.items.length; i++) {
-				// If dropped items aren't files, reject them
-				if (event.dataTransfer.items[i].kind === "file") {
-					const file = event.dataTransfer.items[i].getAsFile();
-					if (file) {
-						if (!event.dataTransfer.items[i].type.startsWith("image")) {
-							file_error = true;
-							file_error_message = "Only image files are supported";
-							files = [];
-							break;
-						}
-						// if image is bigger than 2MB abort
-						if (file.size > 2 * 1024 * 1024) {
-							file_error = true;
-							file_error_message = "Image is too big. (2MB max)";
-							files = [];
-							break;
-						}
-						files = [...files, file];
+			// get only the first file
+			// optionally: we need to handle multiple files, if we want to support document upload for example
+			// for multimodal we only support one image at a time but we could support multiple PDFs
+			if (event.dataTransfer.items[0].kind === "file") {
+				const file = event.dataTransfer.items[0].getAsFile();
+				if (file) {
+					if (!event.dataTransfer.items[0].type.startsWith("image")) {
+						file_error = true;
+						file_error_message = "Only images are supported";
+						files = [];
+						return;
 					}
+					// if image is bigger than 2MB abort
+					if (file.size > 2 * 1024 * 1024) {
+						file_error = true;
+						file_error_message = "Image is too big. (2MB max)";
+						files = [];
+						return;
+					}
+					files = [file];
+					onDrag = false;
 				}
 			}
 		}
-		if (file_error === true) {
-			// Sleep for 2 sec
-			await new Promise((r) => setTimeout(r, 1000));
-		}
-		onDrag = false;
 	}
+
+	$: file_error === true &&
+		setTimeout(() => {
+			file_error = false;
+			onDrag = false;
+		}, 2000);
 </script>
 
 <div
@@ -53,26 +55,43 @@
 	on:drop={dropHandle}
 	class="relative flex w-full max-w-4xl flex-col items-center rounded-xl border bg-gray-100 focus-within:border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:focus-within:border-gray-500"
 >
-	<div class="items-center object-center" />
 	<div class="object-center">
 		{#if file_error}
-			<p class="mb-3 mt-3 text-sm text-gray-500 dark:text-gray-400">
-				Error {file_error_message}
-			</p>
-		{:else}
-			<div class="mt-3 flex justify-center">
-				<CarbonImage class="text-5xl text-gray-500 dark:text-gray-400" />
+			<div
+				class="absolute bottom-0 left-0 right-0 top-0 flex flex-col items-center justify-center gap-2 rounded-xl bg-gray-100 bg-opacity-50 dark:bg-gray-700 dark:bg-opacity-50"
+			>
+				<p class="text-red-500 dark:text-red-400">{file_error_message}</p>
+				<div class="h-2.5 w-1/2 rounded-full bg-gray-200 dark:bg-gray-700">
+					<div
+						class="animate-progress-bar h-2.5
+						rounded-full bg-red-500
+						dark:text-red-400
+					"
+					/>
+				</div>
 			</div>
-			<p class="mb-3 mt-3 text-sm text-gray-500 dark:text-gray-400">
-				Drag and drop your <span class="font-semibold">images</span> here
-			</p>
-			<!-- {:else}
-			{#each fileList as file}
-				<p class="mb-3 mt-3 flex items-center text-sm text-gray-500 dark:text-gray-400">
-					<EosIconsLoading class="mr-2 text-3xl" />
-					<span class="font-semibold">{file.name}</span>: {file.size} bytes is being converted
-				</p>
-			{/each} -->
 		{/if}
+		<div class="mt-3 flex justify-center" class:opacity-0={file_error}>
+			<CarbonImage class="text-5xl text-gray-500 dark:text-gray-400" />
+		</div>
+		<p class="mb-3 mt-3 text-sm text-gray-500 dark:text-gray-400" class:opacity-0={file_error}>
+			Drag and drop <span class="font-semibold">one image</span> here
+		</p>
 	</div>
 </div>
+
+<style>
+	@keyframes slideInFromLeft {
+		0% {
+			width: 0;
+		}
+		100% {
+			width: 100%;
+		}
+	}
+
+	.animate-progress-bar {
+		/* This section calls the slideInFromLeft animation we defined above */
+		animation: 2s linear 0s 1 slideInFromLeft;
+	}
+</style>
