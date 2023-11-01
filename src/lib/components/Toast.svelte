a9f35b53c5c84585a9bdc0444e80e1080262dfe3
Sushil0011 <yashtripathi861@gmail.com>
2023-11-01 11:28:00 +0530

    added save into file option

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -3,12 +3,12 @@
 
 	import IconDazzled from "$lib/components/icons/IconDazzled.svelte";
 
-	export let message = "";
+	export let message = '';
 </script>
 
 <div
 	transition:fade|global={{ duration: 300 }}
-	class="pointer-events-none fixed right-0 top-12 z-20 bg-gradient-to-bl from-red-500/20 via-red-500/0 to-red-500/0 pb-36 pl-36 pr-2 pt-2 md:top-0 md:pr-8 md:pt-5"
+	class="pointer-events-none fixed right-0 bottom-12 z-20 bg-gradient-to-bl from-red-500/20 via-red-500/0 to-red-500/0 pb-36 pl-36 pr-2 pt-2 md:top-0 md:pr-8 md:pt-5"
 >
 	<div
 		class="pointer-events-auto flex items-center rounded-full bg-white/90 px-3 py-1 shadow-sm dark:bg-gray-900/80"
