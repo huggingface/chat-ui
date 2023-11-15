18f333104ebfc36e3507905fc328b243158830e3
Mishig <mishig.davaadorj@coloradocollege.edu>
2023-11-15 01:23:39 -0800

    Update filedropzone (#544)

--- src/lib/components/chat/FileDropzone.svelte
+++ src/lib/components/chat/FileDropzone.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import { onDestroy } from "svelte";
 	import CarbonImage from "~icons/carbon/image";
 	// import EosIconsLoading from "~icons/eos-icons/loading";
 
 	export let files: File[];
 
-	$: file_error = false;
-	$: file_error_message = "";
+	let file_error_message = "";
+	let errorTimeout: ReturnType<typeof setTimeout>;
 
 	export let onDrag = false;
 
@@ -23,15 +24,13 @@
 				const file = event.dataTransfer.items[0].getAsFile();
 				if (file) {
 					if (!event.dataTransfer.items[0].type.startsWith("image")) {
-						file_error = true;
-						file_error_message = "Only images are supported";
+						setErrorMsg("Only images are supported");
 						files = [];
 						return;
 					}
 					// if image is bigger than 2MB abort
 					if (file.size > 2 * 1024 * 1024) {
-						file_error = true;
-						file_error_message = "Image is too big. (2MB max)";
+						setErrorMsg("Image is too big. (2MB max)");
 						files = [];
 						return;
 					}
@@ -42,11 +41,22 @@
 		}
 	}
 
-	$: file_error === true &&
-		setTimeout(() => {
-			file_error = false;
+	function setErrorMsg(errorMsg: string) {
+		if (errorTimeout) {
+			clearTimeout(errorTimeout);
+		}
+		file_error_message = errorMsg;
+		errorTimeout = setTimeout(() => {
+			file_error_message = "";
 			onDrag = false;
 		}, 2000);
+	}
+
+	onDestroy(() => {
+		if (errorTimeout) {
+			clearTimeout(errorTimeout);
+		}
+	});
 </script>
 
 <div
@@ -56,7 +66,7 @@
 	class="relative flex w-full max-w-4xl flex-col items-center rounded-xl border bg-gray-100 focus-within:border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:focus-within:border-gray-500"
 >
 	<div class="object-center">
-		{#if file_error}
+		{#if file_error_message}
 			<div
 				class="absolute bottom-0 left-0 right-0 top-0 flex flex-col items-center justify-center gap-2 rounded-xl bg-gray-100 bg-opacity-50 dark:bg-gray-700 dark:bg-opacity-50"
 			>
@@ -71,10 +81,13 @@
 				</div>
 			</div>
 		{/if}
-		<div class="mt-3 flex justify-center" class:opacity-0={file_error}>
+		<div class="mt-3 flex justify-center" class:opacity-0={file_error_message}>
 			<CarbonImage class="text-5xl text-gray-500 dark:text-gray-400" />
 		</div>
-		<p class="mb-3 mt-3 text-sm text-gray-500 dark:text-gray-400" class:opacity-0={file_error}>
+		<p
+			class="mb-3 mt-3 text-sm text-gray-500 dark:text-gray-400"
+			class:opacity-0={file_error_message}
+		>
 			Drag and drop <span class="font-semibold">one image</span> here
 		</p>
 	</div>
