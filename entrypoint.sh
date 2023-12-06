3fd71d43a48de586278c15c782d05a80b1920253
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-12-06 16:33:06 +0100

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 if test -z "${DOTENV_LOCAL}" ; then
     if ! test -f "/app/config/.env.local" ; then
-        echo "DOTENV_LOCAL was not found in the ENV variables and .env.local is not set using a bind volume, exiting."
-        exit 1
+        echo "DOTENV_LOCAL was not found in the ENV variables and .env.local is not set using a bind volume. We are using the default .env config."
     fi;
 else
     echo "DOTENV_LOCAL was found in the ENV variables. Creating config/.env.local file."
@@ -11,7 +10,8 @@ fi;
 if [ "$USE_LOCAL_DB" = true ] ; then
     echo "USE_LOCAL_DB is set to true. Appending MONGODB_URL"
 
-    "\nMONGODB_URL=mongodb://localhost:27017" >> /app/config/.env.local
+    touch /app/config/.env.local
+    echo -e "\nMONGODB_URL=mongodb://localhost:27017" >> /app/config/.env.local
 
     mkdir -p /data/db
     mongod &
