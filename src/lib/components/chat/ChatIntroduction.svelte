aada23fde9684dcec7cc436f7847830733819377
itaybar <46649219+itaybar@users.noreply.github.com>
2023-12-27 14:49:41 +0200

    make the whole Model div clickable to open settings

--- src/lib/components/chat/ChatIntroduction.svelte
+++ src/lib/components/chat/ChatIntroduction.svelte
@@ -56,20 +56,21 @@
 				> -->
 			</AnnouncementBanner>
 		{/each}
-		<div class="overflow-hidden rounded-xl border dark:border-gray-800">
-			<div class="flex p-3">
-				<div>
-					<div class="text-sm text-gray-600 dark:text-gray-400">Current Model</div>
-					<div class="font-semibold">{currentModel.displayName}</div>
+		<a href="{base}/settings/{currentModel.id}">
+			<div class="overflow-hidden rounded-xl border dark:border-gray-800 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-600">
+				<div class="flex p-3">
+					<div>
+						<div class="text-sm text-gray-600 dark:text-gray-400">Current Model</div>
+						<div class="font-semibold">{currentModel.displayName}</div>
+					</div>
+					<div
+						class="btn ml-auto flex h-7 w-7 self-start rounded-full"
+						><IconGear /></div
+					>
 				</div>
-				<a
-					href="{base}/settings/{currentModel.id}"
-					class="btn ml-auto flex h-7 w-7 self-start rounded-full bg-gray-100 p-1 text-xs hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-600"
-					><IconGear /></a
-				>
+				<!-- <ModelCardMetadata variant="dark" model={currentModel} /> -->
 			</div>
-			<ModelCardMetadata variant="dark" model={currentModel} />
-		</div>
+		</a>
 	</div>
 	{#if currentModelMetadata.promptExamples}
 		<div class="lg:col-span-3 lg:mt-6">
