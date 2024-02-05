f1505b07eddb90261af174c0ba87b49d181d74a2
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-02-05 09:07:12 +0100

    Make the assistant & system prompt button floating on top of screen

--- src/lib/components/chat/ChatMessages.svelte
+++ src/lib/components/chat/ChatMessages.svelte
@@ -45,33 +45,35 @@
 	use:snapScrollToBottom={messages.length ? [...messages, ...webSearchMessages] : false}
 	bind:this={chatContainer}
 >
-	<div class="mx-auto flex h-full max-w-3xl flex-col gap-6 px-5 pt-6 sm:gap-8 xl:max-w-4xl">
+	<div class="mx-auto flex h-full max-w-3xl flex-col gap-6 px-5 pt-12 sm:gap-8 xl:max-w-4xl">
 		{#each messages as message, i}
-			{#if i === 0 && $page.data?.assistant}
-				<a
-					class="mx-auto flex items-center gap-1.5 rounded-full border border-gray-100 bg-gray-50 py-1 pl-1 pr-3 text-sm text-gray-800 hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700"
-					href="{base}/settings/assistants/{$page.data.assistant._id}"
-				>
-					{#if $page.data?.assistant.avatar}
-						<img
-							src="{base}/settings/assistants/{$page.data?.assistant._id.toString()}/avatar.jpg?hash=${$page
-								.data.assistant.avatar}"
-							alt="Avatar"
-							class="size-5 rounded-full object-cover"
-						/>
-					{:else}
-						<div
-							class="flex size-6 items-center justify-center rounded-full bg-gray-300 font-bold uppercase text-gray-500"
-						>
-							{$page.data?.assistant.name[0]}
-						</div>
-					{/if}
+			<div class="absolute left-0 right-0 top-4 z-10 mx-auto w-fit">
+				{#if i === 0 && $page.data?.assistant}
+					<a
+						class="mx-auto flex items-center gap-1.5 rounded-full border border-gray-100 bg-gray-50 py-1 pl-1 pr-3 text-sm text-gray-800 hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700"
+						href="{base}/settings/assistants/{$page.data.assistant._id}"
+					>
+						{#if $page.data?.assistant.avatar}
+							<img
+								src="{base}/settings/assistants/{$page.data?.assistant._id.toString()}/avatar.jpg?hash=${$page
+									.data.assistant.avatar}"
+								alt="Avatar"
+								class="size-5 rounded-full object-cover"
+							/>
+						{:else}
+							<div
+								class="flex size-6 items-center justify-center rounded-full bg-gray-300 font-bold uppercase text-gray-500"
+							>
+								{$page.data?.assistant.name[0]}
+							</div>
+						{/if}
 
-					{$page.data.assistant.name}
-				</a>
-			{:else if i === 0 && preprompt && preprompt != currentModel.preprompt}
-				<SystemPromptModal preprompt={preprompt ?? ""} />
-			{/if}
+						{$page.data.assistant.name}
+					</a>
+				{:else if i === 0 && preprompt && preprompt != currentModel.preprompt}
+					<SystemPromptModal preprompt={preprompt ?? ""} />
+				{/if}
+			</div>
 			<ChatMessage
 				loading={loading && i === messages.length - 1}
 				{message}
