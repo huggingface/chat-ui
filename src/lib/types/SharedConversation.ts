329917982a2160534224dd41828715081b059251
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-02-08 00:12:26 +0100

    fixed shared conv type

--- src/lib/types/SharedConversation.ts
+++ src/lib/types/SharedConversation.ts
@@ -2,7 +2,15 @@ import type { Conversation } from "./Conversation";
 
 export type SharedConversation = Pick<
 	Conversation,
-	"model" | "embeddingModel" | "title" | "rootMessageId" | "messages" | "preprompt" | "assistantId"
+	| "model"
+	| "embeddingModel"
+	| "title"
+	| "rootMessageId"
+	| "messages"
+	| "preprompt"
+	| "assistantId"
+	| "createdAt"
+	| "updatedAt"
 > & {
 	_id: string;
 	hash: string;
