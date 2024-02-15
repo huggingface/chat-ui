e81d4a49b0cad8e4626d366eb1f1a3e5f4d27628
coyotte508 <coyotte508@gmail.com>
2024-02-15 22:46:13 +0100

    🩹 Some fixes

--- src/routes/admin/stats/compute/+server.ts
+++ src/routes/admin/stats/compute/+server.ts
@@ -26,149 +26,152 @@ async function computeStats(params: { dateField: ConversationStats["date"]["fiel
 
 	console.log("Computing stats for", params.dateField, "from", minDay);
 
-	await collections.conversationStats
-		.aggregate([
-			{
-				$match: {
-					[params.dateField]: { $gte: minDay },
-				},
-			},
-			{
-				$project: {
-					[params.dateField]: 1,
-					sessionId: 1,
-					userId: 1,
-				},
-			},
-			{
-				$sort: {
-					[params.dateField]: 1,
-				},
-			},
-			{
-				$facet: {
-					userId: [
-						{
-							$match: {
-								userId: { $exists: true },
-							},
-						},
-						{
-							$group: {
-								_id: {
-									day: { $dateTrunc: { date: `$${params.dateField}`, unit: "day" } },
-									userId: "$userId",
-								},
-								count: { $sum: 1 },
-							},
-						},
-						{
-							$project: {
-								_id: 0,
-								date: {
-									day: "$_id.day",
-									field: params.dateField,
-								},
-								distinct: "userId",
-								count: 1,
-							},
-						},
-					],
-					sessionId: [
-						{
-							$match: {
-								sessionId: { $exists: true },
-							},
-						},
-						{
-							$group: {
-								_id: {
-									day: { $dateTrunc: { date: `$${params.dateField}`, unit: "day" } },
-									sessionId: "$userId",
-								},
-								count: { $sum: 1 },
-							},
-						},
-						{
-							$project: {
-								_id: 0,
-								date: {
-									day: "$_id.day",
-									field: params.dateField,
-								},
-								distinct: "sessionId",
-								count: 1,
-							},
-						},
-					],
-					userOrSessionId: [
-						{
-							$group: {
-								_id: {
-									day: { $dateTrunc: { date: `$${params.dateField}`, unit: "day" } },
-									userOrSessionId: { $ifNull: ["$userId", "$sessionId"] },
-								},
-								count: { $sum: 1 },
-							},
-						},
-						{
-							$project: {
-								_id: 0,
-								date: {
-									day: "$_id.day",
-									field: params.dateField,
-								},
-								distinct: "userOrSessionId",
-								count: 1,
-							},
-						},
-					],
-					_id: [
-						{
-							$group: {
-								_id: {
-									day: { $dateTrunc: { date: `$${params.dateField}`, unit: "day" } },
-								},
-								count: { $sum: 1 },
-							},
-						},
-						{
-							$project: {
-								_id: 0,
-								date: {
-									day: "$_id.day",
-									field: params.dateField,
-								},
-								distinct: "_id",
-								count: 1,
-							},
-						},
-					],
-				},
-			},
-			{
-				$project: {
-					stats: {
-						$concatArrays: ["$userId", "$sessionId", "$userOrSessionId", "$_id"],
+	await collections.conversations
+		.aggregate(
+			[
+				{
+					$match: {
+						[params.dateField]: { $gte: minDay },
 					},
 				},
-			},
-			{
-				$unwind: "$stats",
-			},
-			{
-				$replaceRoot: {
-					newRoot: "$stats",
+				{
+					$project: {
+						[params.dateField]: 1,
+						sessionId: 1,
+						userId: 1,
+					},
 				},
-			},
-			{
-				$merge: {
-					into: CONVERSATION_STATS_COLLECTION,
-					whenMatched: "replace",
-					whenNotMatched: "insert",
+				{
+					$sort: {
+						[params.dateField]: 1,
+					},
 				},
-			},
-		])
+				{
+					$facet: {
+						userId: [
+							{
+								$match: {
+									userId: { $exists: true },
+								},
+							},
+							{
+								$group: {
+									_id: {
+										day: { $dateTrunc: { date: `$${params.dateField}`, unit: "day" } },
+										userId: "$userId",
+									},
+									count: { $sum: 1 },
+								},
+							},
+							{
+								$project: {
+									_id: 0,
+									date: {
+										day: "$_id.day",
+										field: params.dateField,
+									},
+									distinct: "userId",
+									count: 1,
+								},
+							},
+						],
+						sessionId: [
+							{
+								$match: {
+									sessionId: { $exists: true },
+								},
+							},
+							{
+								$group: {
+									_id: {
+										day: { $dateTrunc: { date: `$${params.dateField}`, unit: "day" } },
+										sessionId: "$userId",
+									},
+									count: { $sum: 1 },
+								},
+							},
+							{
+								$project: {
+									_id: 0,
+									date: {
+										day: "$_id.day",
+										field: params.dateField,
+									},
+									distinct: "sessionId",
+									count: 1,
+								},
+							},
+						],
+						userOrSessionId: [
+							{
+								$group: {
+									_id: {
+										day: { $dateTrunc: { date: `$${params.dateField}`, unit: "day" } },
+										userOrSessionId: { $ifNull: ["$userId", "$sessionId"] },
+									},
+									count: { $sum: 1 },
+								},
+							},
+							{
+								$project: {
+									_id: 0,
+									date: {
+										day: "$_id.day",
+										field: params.dateField,
+									},
+									distinct: "userOrSessionId",
+									count: 1,
+								},
+							},
+						],
+						_id: [
+							{
+								$group: {
+									_id: {
+										day: { $dateTrunc: { date: `$${params.dateField}`, unit: "day" } },
+									},
+									count: { $sum: 1 },
+								},
+							},
+							{
+								$project: {
+									_id: 0,
+									date: {
+										day: "$_id.day",
+										field: params.dateField,
+									},
+									distinct: "_id",
+									count: 1,
+								},
+							},
+						],
+					},
+				},
+				{
+					$project: {
+						stats: {
+							$concatArrays: ["$userId", "$sessionId", "$userOrSessionId", "$_id"],
+						},
+					},
+				},
+				{
+					$unwind: "$stats",
+				},
+				{
+					$replaceRoot: {
+						newRoot: "$stats",
+					},
+				},
+				{
+					$merge: {
+						into: CONVERSATION_STATS_COLLECTION,
+						whenMatched: "replace",
+						whenNotMatched: "insert",
+					},
+				},
+			],
+			{ allowDiskUse: true }
+		)
 		.next();
 
 	console.log("Computed stats for", params.dateField);
