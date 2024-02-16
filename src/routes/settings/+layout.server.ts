12dd66dd22402a5c8b50de9fd89d9e939d76651a
Mishig <mishig.davaadorj@coloradocollege.edu>
2024-02-16 08:10:09 -0800

    Revert "[Mongo] Optimize `reports` collection query (#832)" (#833)
    This reverts commit 19329abf32a4f49ff048585d0f6df5887cea8465.

--- src/routes/settings/+layout.server.ts
+++ src/routes/settings/+layout.server.ts
@@ -1,7 +1,6 @@
 import { collections } from "$lib/server/database";
 import { ObjectId } from "mongodb";
 import type { LayoutServerLoad } from "./$types";
-import type { Report } from "$lib/types/Report";
 
 export const load = (async ({ locals, parent }) => {
 	const { settings } = await parent();
@@ -13,22 +12,20 @@ export const load = (async ({ locals, parent }) => {
 		})
 		.toArray();
 
-	let reportsByUser: ObjectId[] = [];
-	const createdBy = locals.user?._id ?? locals.sessionId;
-	if (createdBy) {
-		const reports = await collections.reports
-			.find<Pick<Report, "assistantId">>({ createdBy }, { projection: { _id: 0, assistantId: 1 } })
-			.toArray();
-		reportsByUser = reports.map((r) => r.assistantId);
-	}
-
 	return {
-		assistants: assistants.map((el) => ({
-			...el,
-			_id: el._id.toString(),
-			createdById: undefined,
-			createdByMe: el.createdById.toString() === (locals.user?._id ?? locals.sessionId).toString(),
-			reported: reportsByUser.includes(el._id),
-		})),
+		assistants: await Promise.all(
+			assistants.map(async (el) => ({
+				...el,
+				_id: el._id.toString(),
+				createdById: undefined,
+				createdByMe:
+					el.createdById.toString() === (locals.user?._id ?? locals.sessionId).toString(),
+				reported:
+					(await collections.reports.countDocuments({
+						assistantId: el._id,
+						createdBy: locals.user?._id ?? locals.sessionId,
+					})) > 0,
+			}))
+		),
 	};
 }) satisfies LayoutServerLoad;
