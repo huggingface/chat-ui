10408b550eea90f8c19fc0446628945e083d203b
KartikGS <starkartik9@gmail.com>
2024-02-19 20:21:25 +0530

    report modal dark mode

--- src/routes/settings/assistants/[assistantId]/ReportModal.svelte
+++ src/routes/settings/assistants/[assistantId]/ReportModal.svelte
@@ -20,17 +20,17 @@
 				invalidateAll();
 			};
 		}}
-		class="w-full min-w-64 p-4"
+		class="w-full min-w-64 p-4 dark:bg-gray-900"
 	>
-		<span class="mb-1 text-sm font-semibold">Report an assistant</span>
+		<span class="mb-1 text-sm font-semibold dark:text-gray-300">Report an assistant</span>
 
-		<p class="text-sm text-gray-500">
+		<p class="text-sm text-gray-500 dark:text-gray-400">
 			Please provide a brief description of why you are reporting this assistant.
 		</p>
 
 		<textarea
 			name="reportReason"
-			class="mt-6 max-h-48 w-full resize-y rounded-lg border-2 border-gray-200 bg-gray-100 p-2 text-smd"
+			class="mt-6 max-h-48 w-full resize-y rounded-lg border-2 border-gray-200 bg-gray-100 p-2 text-smd dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300"
 			placeholder="Reason(s) for the report"
 			maxlength="128"
 			bind:value={reason}
@@ -39,15 +39,16 @@
 		<div class="flex w-full flex-row justify-between px-2 pt-4">
 			<button
 				type="button"
-				class="text-sm text-gray-700 hover:underline"
+				class="text-sm text-gray-700 hover:underline dark:text-gray-300"
 				on:click={() => dispatch("close")}>Cancel</button
 			>
 
 			<button
 				type="submit"
-				class="rounded-full bg-black px-4 py-2 text-sm font-semibold text-white md:px-8"
+				class="rounded-full bg-black px-4 py-2 text-sm font-semibold text-white md:px-8 dark:text-gray-900 dark:bg-gray-300"
 				disabled={!reason}
 				class:bg-gray-200={!reason}
+				class:dark:bg-gray-600={!reason}
 				class:!text-gray-400={!reason}
 			>
 				Submit report
