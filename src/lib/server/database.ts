634bd6968bef1740119322755cc468ea824b9d14
Eliott C <coyotte508@gmail.com>
2024-02-20 11:13:50 +0100

    ✨ Add stats on conversations (#828)
    * ✨ Add stats on conversations
    
    * 🩹 Fix conversation stats compute
    
    * ⚡️ Slightly better index
    
    * 🔊
    
    * 🩹 Some fixes
    
    * 🐛 Fix aggregation query
    
    * 🐛 Fix $merge stage of query
    
    * 🐛 Fix query on sessionId
    
    * ✨ Compute weekly/monthly unique users
    
    * 🩹 Final tweaks
    
    * ⚡️ Split aggregations for better perf
    
    No need to recompute DAUs / WAUs and so on
    
    * ♻️ Deprecate PARQUET_EXPORT_SECRET & fix sec vuln
    
    * Add ADMIN_API_SECRET to CD action
    
    ---------
    
    Co-authored-by: Nathan Sarrazin <sarrazin.nathan@gmail.com>

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -9,6 +9,7 @@ import type { MessageEvent } from "$lib/types/MessageEvent";
 import type { Session } from "$lib/types/Session";
 import type { Assistant } from "$lib/types/Assistant";
 import type { Report } from "$lib/types/Report";
+import type { ConversationStats } from "$lib/types/ConversationStats";
 
 if (!MONGODB_URL) {
 	throw new Error(
@@ -24,7 +25,10 @@ export const connectPromise = client.connect().catch(console.error);
 
 const db = client.db(MONGODB_DB_NAME + (import.meta.env.MODE === "test" ? "-test" : ""));
 
+export const CONVERSATION_STATS_COLLECTION = "conversations.stats";
+
 const conversations = db.collection<Conversation>("conversations");
+const conversationStats = db.collection<ConversationStats>(CONVERSATION_STATS_COLLECTION);
 const assistants = db.collection<Assistant>("assistants");
 const reports = db.collection<Report>("reports");
 const sharedConversations = db.collection<SharedConversation>("sharedConversations");
@@ -38,6 +42,7 @@ const bucket = new GridFSBucket(db, { bucketName: "files" });
 export { client, db };
 export const collections = {
 	conversations,
+	conversationStats,
 	assistants,
 	reports,
 	sharedConversations,
@@ -67,6 +72,33 @@ client.on("open", () => {
 			{ "message.id": 1, "message.ancestors": 1 },
 			{ partialFilterExpression: { userId: { $exists: true } } }
 		)
+		.catch(console.error);
+	// To do stats on conversations
+	conversations.createIndex({ updatedAt: 1 }).catch(console.error);
+	// Not strictly necessary, could use _id, but more convenient. Also for stats
+	conversations.createIndex({ createdAt: 1 }).catch(console.error);
+	// To do stats on conversation messages
+	conversations.createIndex({ "messages.createdAt": 1 }, { sparse: true }).catch(console.error);
+	// Unique index for stats
+	conversationStats
+		.createIndex(
+			{
+				type: 1,
+				"date.field": 1,
+				"date.span": 1,
+				"date.at": 1,
+				distinct: 1,
+			},
+			{ unique: true }
+		)
+		.catch(console.error);
+	// Allow easy check of last computed stat for given type/dateField
+	conversationStats
+		.createIndex({
+			type: 1,
+			"date.field": 1,
+			"date.at": 1,
+		})
 		.catch(console.error);
 	abortedGenerations.createIndex({ updatedAt: 1 }, { expireAfterSeconds: 30 }).catch(console.error);
 	abortedGenerations.createIndex({ conversationId: 1 }, { unique: true }).catch(console.error);
--- src/routes/admin/export/+server.ts
+++ src/routes/admin/export/+server.ts
@@ -1,8 +1,4 @@
-import {
-	PARQUET_EXPORT_DATASET,
-	PARQUET_EXPORT_HF_TOKEN,
-	PARQUET_EXPORT_SECRET,
-} from "$env/static/private";
+import { PARQUET_EXPORT_DATASET, PARQUET_EXPORT_HF_TOKEN } from "$env/static/private";
 import { collections } from "$lib/server/database";
 import type { Message } from "$lib/types/Message";
 import { error } from "@sveltejs/kit";
@@ -13,15 +9,11 @@ import parquet from "parquetjs";
 import { z } from "zod";
 
 // Triger like this:
-// curl -X POST "http://localhost:5173/chat/admin/export" -H "Authorization: Bearer <PARQUET_EXPORT_SECRET>" -H "Content-Type: application/json" -d '{"model": "OpenAssistant/oasst-sft-6-llama-30b-xor"}'
+// curl -X POST "http://localhost:5173/chat/admin/export" -H "Authorization: Bearer <ADMIN_API_SECRET>" -H "Content-Type: application/json" -d '{"model": "OpenAssistant/oasst-sft-6-llama-30b-xor"}'
 
 export async function POST({ request }) {
-	if (!PARQUET_EXPORT_SECRET || !PARQUET_EXPORT_DATASET || !PARQUET_EXPORT_HF_TOKEN) {
+	if (!PARQUET_EXPORT_DATASET || !PARQUET_EXPORT_HF_TOKEN) {
 		throw error(500, "Parquet export is not configured.");
-	}
-
-	if (request.headers.get("Authorization") !== `Bearer ${PARQUET_EXPORT_SECRET}`) {
-		throw error(403);
 	}
 
 	const { model } = z
