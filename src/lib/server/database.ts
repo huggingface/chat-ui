294f6d96828d41e553bdbd2f3b80dc2c61d5b2e4
Mishig <mishig.davaadorj@coloradocollege.edu>
2024-03-05 08:06:47 -0800

    [Assistants] Filter on names (#841)
    * [Assistants] Filter on names
    
    * Add `$text` index on `assistant.name` (#844)
    
    * add maxlength
    
    * better experience
    
    * use `$meta: "textScore"`
    
    * Update src/routes/assistants/+page.server.ts
    
    Co-authored-by: Eliott C. <coyotte508@gmail.com>
    
    * null, not undefined
    
    * [Assistants] Filter on names (using searchTokens) (#873)
    
    Filter with `searchTokens`
    
    * input
    
    * rm extra whitespace
    
    * hide UI before migration
    
    * rm ad-hoc migration
    
    ---------
    
    Co-authored-by: Eliott C. <coyotte508@gmail.com>
    Co-authored-by: Nathan Sarrazin <sarrazin.nathan@gmail.com>
    Co-authored-by: Victor Mustar <victor.mustar@gmail.com>

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -117,6 +117,7 @@ client.on("open", () => {
 	assistants.createIndex({ userCount: 1 }).catch(console.error);
 	assistants.createIndex({ featured: 1, userCount: -1 }).catch(console.error);
 	assistants.createIndex({ modelId: 1, userCount: -1 }).catch(console.error);
+	assistants.createIndex({ searchTokens: 1 }).catch(console.error);
 	reports.createIndex({ assistantId: 1 }).catch(console.error);
 	reports.createIndex({ createdBy: 1, assistantId: 1 }).catch(console.error);
 });
--- src/routes/assistants/+page.server.ts
+++ src/routes/assistants/+page.server.ts
@@ -3,6 +3,7 @@ import { ENABLE_ASSISTANTS } from "$env/static/private";
 import { collections } from "$lib/server/database.js";
 import type { Assistant } from "$lib/types/Assistant";
 import type { User } from "$lib/types/User";
+import { generateQueryTokens } from "$lib/utils/searchTokens.js";
 import { error, redirect } from "@sveltejs/kit";
 import type { Filter } from "mongodb";
 
@@ -16,6 +17,7 @@ export const load = async ({ url, locals }) => {
 	const modelId = url.searchParams.get("modelId");
 	const pageIndex = parseInt(url.searchParams.get("p") ?? "0");
 	const username = url.searchParams.get("user");
+	const query = url.searchParams.get("q")?.trim() ?? null;
 	const createdByCurrentUser = locals.user?.username && locals.user.username === username;
 
 	let user: Pick<User, "_id"> | null = null;
@@ -34,6 +36,7 @@ export const load = async ({ url, locals }) => {
 		...(modelId && { modelId }),
 		...(!createdByCurrentUser && { userCount: { $gt: 1 } }),
 		...(user ? { createdById: user._id } : { featured: true }),
+		...(query && { searchTokens: { $all: generateQueryTokens(query) } }),
 	};
 	const assistants = await collections.assistants
 		.find(filter)
@@ -49,5 +52,6 @@ export const load = async ({ url, locals }) => {
 		selectedModel: modelId ?? "",
 		numTotalItems,
 		numItemsPerPage: NUM_PER_PAGE,
+		query,
 	};
 };
