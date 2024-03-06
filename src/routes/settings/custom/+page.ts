ed48a75971dea7d412feeacd9d9fda951c6e2d26
KartikGS <starkartik9@gmail.com>
2024-03-06 17:34:50 +0530

    lint error correction

--- src/routes/settings/custom/+page.ts
+++ src/routes/settings/custom/+page.ts
@@ -1,7 +1,4 @@
-import { base } from "$app/paths";
-import { redirect } from "@sveltejs/kit";
-
-export async function load({ parent, params }) {
+export async function load({ parent }) {
 	const data = await parent();
 
 	return data;
