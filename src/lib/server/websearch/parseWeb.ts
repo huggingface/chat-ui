6f822dcd793e2fa91b9689f6523f13db68d43f27
Victor Mustar <victor.mustar@gmail.com>
2024-03-14 15:58:40 +0100

    add ul and ol to parseWeb

--- src/lib/server/websearch/parseWeb.ts
+++ src/lib/server/websearch/parseWeb.ts
@@ -17,7 +17,7 @@ export async function parseWeb(url: string) {
 		});
 
 		const { document } = dom.window;
-		const paragraphs = document.querySelectorAll("p, table, pre");
+		const paragraphs = document.querySelectorAll("p, table, pre, ul, ol");
 
 		if (!paragraphs.length) {
 			throw new Error(`webpage doesn't have any parseable element`);
