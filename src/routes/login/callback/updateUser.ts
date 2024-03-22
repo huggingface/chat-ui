c6129c38dcae1ab7c5e2c1c257d8545ddbac991a
Chang Chi, Meng <daniel2000890311g@gmail.com>
2024-03-22 17:25:10 +0800

    Enhance Dynamic User Attribute Handling in OIDC Integration (#885)
    * Refactor `updateUser` to Support Dynamic `nameClaim` Configuration
    
    * style: run Prettier to fix lint errors
    
    * Validate NAME_CLAIM in OIDConfig schema
    
    * Refactor userData parsing
    
    ---------
    
    Co-authored-by: Nathan Sarrazin <sarrazin.nathan@gmail.com>
    Co-authored-by: ‘XGungo’ <‘daniel2000890311g@gmail.com’>

--- src/routes/login/callback/updateUser.ts
+++ src/routes/login/callback/updateUser.ts
@@ -8,6 +8,7 @@ import { error, type Cookies } from "@sveltejs/kit";
 import crypto from "crypto";
 import { sha256 } from "$lib/utils/sha256";
 import { addWeeks } from "date-fns";
+import { OIDConfig } from "$lib/server/auth";
 
 export async function updateUser(params: {
 	userData: UserinfoResponse;
@@ -38,10 +39,24 @@ export async function updateUser(params: {
 			sub: z.string(),
 			email: z.string().email().optional(),
 		})
+		.setKey(OIDConfig.NAME_CLAIM, z.string())
 		.refine((data) => data.preferred_username || data.email, {
 			message: "Either preferred_username or email must be provided by the provider.",
 		})
-		.parse(userData);
+		.transform((data) => ({
+			...data,
+			name: data[OIDConfig.NAME_CLAIM],
+		}))
+		.parse(userData) as {
+		preferred_username?: string;
+		email?: string;
+		picture?: string;
+		sub: string;
+		name: string;
+	} & Record<string, string>;
+
+	// Dynamically access user data based on NAME_CLAIM from environment
+	// This approach allows us to adapt to different OIDC providers flexibly.
 
 	// check if user already exists
 	const existingUser = await collections.users.findOne({ hfUserId });
