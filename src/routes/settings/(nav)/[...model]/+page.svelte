841da29d0841b0f9a81a38607c93a546fa1006c5
Mishig <mishig.davaadorj@coloradocollege.edu>
2024-03-22 05:51:43 -0700

    Count system prompt tokens (#850)
    * Count sysmte prompt tokens
    
    * show error only once
    
    * fix
    
    * fix css
    
    * simplify
    
    * fix
    
    * simplify
    
    * Revert "simplify"
    
    This reverts commit 61c0a2281f9795c4a4351d9ad473993a3c01c59f.
    
    * `model.tokenizer` config & fix reactivity issues
    
    * rm gated tokenizer
    
    * use `truncate`
    
    ---------
    
    Co-authored-by: Nathan Sarrazin <sarrazin.nathan@gmail.com>

--- src/routes/settings/(nav)/[...model]/+page.svelte
+++ src/routes/settings/(nav)/[...model]/+page.svelte
@@ -5,6 +5,7 @@
 	import type { BackendModel } from "$lib/server/models";
 	import { useSettingsStore } from "$lib/stores/settings";
 	import CopyToClipBoardBtn from "$lib/components/CopyToClipBoardBtn.svelte";
+	import TokensCounter from "$lib/components/TokensCounter.svelte";
 	import CarbonArrowUpRight from "~icons/carbon/arrow-up-right";
 	import CarbonLink from "~icons/carbon/link";
 
@@ -99,7 +100,7 @@
 		{isActive ? "Active model" : "Activate"}
 	</button>
 
-	<div class="flex w-full flex-col gap-2">
+	<div class="relative flex w-full flex-col gap-2">
 		<div class="flex w-full flex-row content-between">
 			<h3 class="mb-1.5 text-lg font-semibold text-gray-800">System Prompt</h3>
 			{#if hasCustomPreprompt}
@@ -117,5 +118,13 @@
 			class="w-full resize-none rounded-md border-2 bg-gray-100 p-2"
 			bind:value={$settings.customPrompts[$page.params.model]}
 		/>
+		{#if model.tokenizer && $settings.customPrompts[$page.params.model]}
+			<TokensCounter
+				classNames="absolute bottom-2 right-2"
+				prompt={$settings.customPrompts[$page.params.model]}
+				modelTokenizer={model.tokenizer}
+				truncate={model?.parameters?.truncate}
+			/>
+		{/if}
 	</div>
 </div>
