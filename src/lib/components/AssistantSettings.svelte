f0a3399df60a00cab21691fc9f002c2fd94e27fa
Victor Mustar <victor.mustar@gmail.com>
2024-03-25 14:28:26 +0100

    Revert "trying something"
    This reverts commit e30ab33f801fad31cbcaa7b89c47c79d4aca86a6.

--- src/lib/components/AssistantSettings.svelte
+++ src/lib/components/AssistantSettings.svelte
@@ -314,7 +314,7 @@
 						{/if}
 					</span>
 
-					<label class="mt-1">
+					<label class="mt-1" class:hidden={dynamicPrompt}>
 						<input
 							checked={!ragMode}
 							on:change={() => (ragMode = false)}
@@ -322,14 +322,11 @@
 							name="ragMode"
 							value={false}
 						/>
-						<span class="my-2 text-sm" class:font-semibold={!ragMode}>
-							{dynamicPrompt ? "Enabled by Dynamic Prompt" : "Disabled"}
-						</span>
+						<span class="my-2 text-sm" class:font-semibold={!ragMode}> Disabled </span>
 						{#if !ragMode}
 							<span class="block text-xs text-gray-500">
-								{dynamicPrompt
-									? "Dynamic prompt can connect this Assistant to external services."
-									: "Assistant won't look for information from Internet and will be faster to answer. Recommended for most Assistants."}
+								Assistant won't look for information from Internet and will be faster to answer.
+								Recommended for most Assistants.
 							</span>
 						{/if}
 					</label>
