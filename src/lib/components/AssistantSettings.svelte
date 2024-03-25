aae4b833e4b7d1f31948bcc9dd5ac8fe189400ff
Victor Mustar <victor.mustar@gmail.com>
2024-03-25 13:06:44 +0100

    regex update

--- src/lib/components/AssistantSettings.svelte
+++ src/lib/components/AssistantSettings.svelte
@@ -89,7 +89,7 @@
 		? "domains"
 		: false;
 
-	const regex = /\{\{\s*url\s*=\s*(.*?)\s*\}\}/g;
+	const regex = /\{\{\s*url\s*=\s*(.+?)\s*\}\}/g;
 	$: templateVariables = [...systemPrompt.matchAll(regex)].map((match) => match[1]);
 </script>
 
