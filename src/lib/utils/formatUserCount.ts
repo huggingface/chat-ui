eb956bc77515f2d25956f6db7842814449363a00
Victor Muštar <victor.mustar@gmail.com>
2024-04-02 21:47:09 +0200

    User count update (#970)
    * add more range
    
    * display on assitant page too

--- src/lib/utils/formatUserCount.ts
+++ src/lib/utils/formatUserCount.ts
@@ -10,7 +10,11 @@ export function formatUserCount(userCount: number): string {
 		{ min: 1_000, max: 2_999, label: "1k+" },
 		{ min: 3_000, max: 4_999, label: "3k+" },
 		{ min: 5_000, max: 9_999, label: "5k+" },
-		{ min: 10_000, max: Infinity, label: "10k+" },
+		{ min: 10_000, max: 19_999, label: "10k+" },
+		{ min: 20_000, max: 29_999, label: "20k+" },
+		{ min: 30_000, max: 39_999, label: "30k+" },
+		{ min: 40_000, max: 49_999, label: "40k+" },
+		{ min: 50_000, max: Infinity, label: "50k+" },
 	];
 
 	const range = userCountRanges.find(({ min, max }) => userCount >= min && userCount <= max);
