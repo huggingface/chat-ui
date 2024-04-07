c256214e1913cbbc40440f215ae7e80ad65efad9
Zac Pullar-Strecker <zac.pullar-strecker@lanzatech.com>
2024-04-08 10:36:41 +1200

    Undo relative paths

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,6 @@ const config = {
 
 		paths: {
 			base: process.env.APP_BASE || "",
-			relative: true,
 		},
 		csrf: {
 			// handled in hooks.server.ts, because we can have multiple valid origins
