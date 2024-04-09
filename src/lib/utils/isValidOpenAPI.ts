6e31d7266844156df5b835cb2023542e1e834f65
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-04-09 12:04:43 +0200

    smol

--- src/lib/utils/isValidOpenAPI.ts
+++ src/lib/utils/isValidOpenAPI.ts
@@ -1,12 +1,13 @@
 // import OpenAPIParser from "@apidevtools/swagger-parser";
 
-// export function isValidOpenAPI(spec: string) {
-// 	// OpenAPIParser.validate(spec, (err) => {
-// 	// 	if (err) {
-// 	// 		console.log(err);
-// 	// 	}
-// 	return true;
-// 	// TODO:  BRING THIS BACK
-// 	// return err ? err.message : true;
-// 	// });
-// }
+export function isValidOpenAPI(spec: string) {
+	console.log(spec);
+	// OpenAPIParser.validate(spec, (err) => {
+	// 	if (err) {
+	// 		console.log(err);
+	// 	}
+	return true;
+	// TODO:  BRING THIS BACK
+	// return err ? err.message : true;
+	// });
+}
