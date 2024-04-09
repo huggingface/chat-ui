4f3716b4331e524a3c50fb4efcff020a82c0a5ab
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-04-09 12:02:26 +0200

    lint

--- src/lib/server/tools/callFromSpecAndParams.ts
+++ src/lib/server/tools/callFromSpecAndParams.ts
@@ -1,13 +1,13 @@
-import type { OpenAPIV3_1 } from "openapi-types";
+// import type { OpenAPIV3_1 } from "openapi-types";
 
-export async function callOperationFromSpecAndParams(
-	apiSpec: OpenAPIV3_1.Document,
-	operationId: string,
-	parameters: Record<string, string | boolean | number>
-): Promise<string> {
-	// find the matching operationId in the spec, validate parameters against its inputs in the apiSPec
-	// then fill in proper parameters, making sure to support :
-	// path parameters, such as /users/{id}
-	// query parameters, such as /users?role=admin
-	// and the POST
-}
+// export async function callOperationFromSpecAndParams(
+// 	apiSpec: OpenAPIV3_1.Document,
+// 	operationId: string,
+// 	parameters: Record<string, string | boolean | number>
+// ): Promise<string> {
+// 	// find the matching operationId in the spec, validate parameters against its inputs in the apiSPec
+// 	// then fill in proper parameters, making sure to support :
+// 	// path parameters, such as /users/{id}
+// 	// query parameters, such as /users?role=admin
+// 	// and the POST
+// }
--- src/lib/utils/isValidOpenAPI.ts
+++ src/lib/utils/isValidOpenAPI.ts
@@ -1,12 +1,12 @@
-import OpenAPIParser from "@apidevtools/swagger-parser";
+// import OpenAPIParser from "@apidevtools/swagger-parser";
 
-export function isValidOpenAPI(spec: string) {
-	// OpenAPIParser.validate(spec, (err) => {
-	// 	if (err) {
-	// 		console.log(err);
-	// 	}
-	return true;
-	// TODO:  BRING THIS BACK
-	// return err ? err.message : true;
-	// });
-}
+// export function isValidOpenAPI(spec: string) {
+// 	// OpenAPIParser.validate(spec, (err) => {
+// 	// 	if (err) {
+// 	// 		console.log(err);
+// 	// 	}
+// 	return true;
+// 	// TODO:  BRING THIS BACK
+// 	// return err ? err.message : true;
+// 	// });
+// }
