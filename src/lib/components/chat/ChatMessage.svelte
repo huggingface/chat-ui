ea801a7dd5be276a44589b93d3dbf80de7aab342
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-04-15 12:33:29 +0200

    show retry even if no message

--- src/lib/components/chat/ChatMessage.svelte
+++ src/lib/components/chat/ChatMessage.svelte
@@ -313,7 +313,7 @@
 				</div>
 			{/if}
 		</div>
-		{#if !loading && message.content}
+		{#if !loading && (message.content || tools)}
 			<div
 				class="absolute bottom-1 right-0 -mb-4 flex max-md:transition-all md:bottom-0 md:group-hover:visible md:group-hover:opacity-100
 		{message.score ? 'visible opacity-100' : 'invisible max-md:-translate-y-4 max-md:opacity-0'}
