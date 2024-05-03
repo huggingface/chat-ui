dfbf9d99a6381abe453dff648ce283b7b12be0bd
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-05-03 12:50:22 +0200

    add version check

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,4 +14,6 @@ if [ "$INCLUDE_DB" = "true" ] ; then
     nohup mongod &
 fi;
 
+export PUBLIC_VERSION=$(node -p "require('./package.json').version")
+
 dotenv -e /app/.env -c -- node /app/build/index.js -- --host 0.0.0.0 --port 3000
