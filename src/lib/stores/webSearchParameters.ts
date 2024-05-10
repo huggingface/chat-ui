ab6fbfbd07ac2eac8196160b372d41031007fa48
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-05-09 23:19:30 -0600

    moar cleanup

--- src/lib/stores/webSearchParameters.ts
+++ src/lib/stores/webSearchParameters.ts
@@ -1,11 +1,9 @@
 import { writable } from "svelte/store";
 export interface WebSearchParameters {
 	useSearch: boolean;
-	useTools: boolean;
 	nItems: number;
 }
 export const webSearchParameters = writable<WebSearchParameters>({
 	useSearch: false,
-	useTools: true, // since the model will decide whether or not to use tools, it makes sense to default to true here
 	nItems: 5,
 });
