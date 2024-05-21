67bc262257303586ac0d74eb7f8b10c49ba0950c
Liam Dyer <liamcdyer@gmail.com>
2024-05-21 12:30:03 -0400

    feat: add index to tools settings

--- src/lib/migrations/routines/03-add-tools-in-settings.ts
+++ src/lib/migrations/routines/03-add-tools-in-settings.ts
@@ -1,6 +1,7 @@
 import type { Migration } from ".";
 import { collections } from "$lib/server/database";
 import { ObjectId } from "mongodb";
+import { logger } from "$lib/server/logger";
 
 const addToolsToSettings: Migration = {
 	_id: new ObjectId("5c9c4c4c4c4c4c4c4c4c4c4c"),
@@ -15,6 +16,10 @@ const addToolsToSettings: Migration = {
 			},
 			{ $set: { tools: {} } }
 		);
+
+		settings
+			.createIndex({ tools: 1 })
+			.catch((e) => logger.error("Error creating index during tools migration", e));
 
 		return true;
 	},
