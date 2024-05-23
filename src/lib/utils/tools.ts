4ac302ced4d9a8ddf34d021311a9018b12ad36a2
Liam Dyer <liamcdyer@gmail.com>
2024-05-23 10:23:46 -0400

    feat: resolve type error

--- src/lib/utils/tools.ts
+++ src/lib/utils/tools.ts
@@ -4,6 +4,6 @@ import type { Tool } from "$lib/types/Tool";
  * Checks if a tool's name equals a value. Replaces all hyphens with underscores before comparison
  * since some models return underscores even when hyphens are used in the request.
  **/
-export function toolHasName(name: string, tool: Tool): boolean {
+export function toolHasName(name: string, tool: Pick<Tool, "name">): boolean {
 	return tool.name.replaceAll("-", "_") === name.replaceAll("-", "_");
 }
