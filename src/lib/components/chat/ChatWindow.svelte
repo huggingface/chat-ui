276715eb2a8f6a04d8a48e5354bde0adc848079c
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-06-03 13:00:07 +0200

    Hide upload button when tools are not available (#1245)
    * Hide upload button when tools are not available
    
    * lint

--- src/lib/components/chat/ChatWindow.svelte
+++ src/lib/components/chat/ChatWindow.svelte
@@ -158,7 +158,9 @@
 		(tool: ToolFront) => $settings?.tools?.[tool.name] ?? tool.isOnByDefault
 	);
 	$: activeMimeTypes = [
-		...activeTools.flatMap((tool: ToolFront) => tool.mimeTypes ?? []),
+		...(!$page.data?.assistant && currentModel.tools
+			? activeTools.flatMap((tool: ToolFront) => tool.mimeTypes ?? [])
+			: []),
 		...(currentModel.multimodal ? ["image/*"] : []),
 	];
 </script>
