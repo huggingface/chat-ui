854f9f74f70510e62148265e891fb20500d8939a
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-06-03 11:51:50 +0200

    make it hover only

--- src/lib/components/chat/ChatMessage.svelte
+++ src/lib/components/chat/ChatMessage.svelte
@@ -513,7 +513,7 @@
 		<svelte:fragment slot="childrenNav">
 			{#if nChildren > 1 && $convTreeStore.editing === null}
 				<div
-					class="font-white z-10 -mt-1 ml-3.5 mr-auto flex h-6 w-fit select-none flex-row items-center justify-center gap-1 text-sm"
+					class="font-white group/navbranch z-10 -mt-1 ml-3.5 mr-auto flex h-6 w-fit select-none flex-row items-center justify-center gap-1 text-sm"
 				>
 					<button
 						class="inline text-lg font-thin text-gray-400 disabled:pointer-events-none disabled:opacity-25 hover:text-gray-800 dark:text-gray-500 dark:hover:text-gray-200"
@@ -536,7 +536,11 @@
 					>
 						<CarbonChevronRight class="text-sm" />
 					</button>
-					{#if !loading && message.children}<form method="POST" action="?/deleteBranch">
+					{#if !loading && message.children}<form
+							method="POST"
+							action="?/deleteBranch"
+							class="hidden group-hover/navbranch:block"
+						>
 							<input name="messageId" value={message.children[childrenToRender]} type="hidden" />
 							<button
 								class="flex items-center justify-center text-xs text-gray-400 hover:text-gray-800 dark:text-gray-500 dark:hover:text-gray-200"
