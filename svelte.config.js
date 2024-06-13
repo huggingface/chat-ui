2e95674dc5821f3f891001d05873c6053b0784bf
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-06-13 10:44:23 +0200

    chore(deps): update major versions on svelte, sveltekit & node (#1281)
    * Bump versions
    
    * fix checks

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from "@sveltejs/adapter-node";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "./.env.local" });
