15200e73786d32318929df87cab43cc6e85aa267
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-06-24 09:58:37 +0000

    lint

--- src/lib/components/chat/ChatIntroduction.svelte
+++ src/lib/components/chat/ChatIntroduction.svelte
@@ -86,7 +86,7 @@
 				{#each currentModelMetadata.promptExamples as example}
 					<button
 						type="button"
-						class="rounded-xl border bg-gray-50 p-3 text-gray-600 hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 max-xl:text-sm xl:p-3.5"
+						class="rounded-xl border bg-gray-50 p-3 text-gray-600 hover:bg-gray-100 max-xl:text-sm xl:p-3.5 dark:border-gray-800 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700"
 						on:click={() => dispatch("message", example.prompt)}
 					>
 						{example.title}
