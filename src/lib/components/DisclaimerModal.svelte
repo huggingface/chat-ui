6ec1e8fa5182fe9604a80e25e9bee4d5775bca99
Kirill Kazakov <kazakovkirill1@gmail.com>
2024-06-27 19:20:46 +0100

    Fix "login" button text wrap on mobile

--- src/lib/components/DisclaimerModal.svelte
+++ src/lib/components/DisclaimerModal.svelte
@@ -54,11 +54,14 @@
 				<form action="{base}/login" target="_parent" method="POST" class="w-full">
 					<button
 						type="submit"
-						class="flex w-full items-center justify-center whitespace-nowrap rounded-full border-2 border-black bg-black px-5 py-2 text-lg font-semibold text-gray-100 transition-colors hover:bg-gray-900"
+						class="flex w-full flex-wrap items-center justify-center whitespace-nowrap rounded-full border-2 border-black bg-black px-5 py-2 text-lg font-semibold text-gray-100 transition-colors hover:bg-gray-900"
 					>
 						Sign in
 						{#if envPublic.PUBLIC_APP_NAME === "HuggingChat"}
-							with <LogoHuggingFaceBorderless classNames="text-xl mr-1 ml-1.5 flex-none" /> Hugging Face
+							<span class="flex items-center">
+								&nbsp;with <LogoHuggingFaceBorderless classNames="text-xl mr-1 ml-1.5 flex-none" /> Hugging
+								Face
+							</span>
 						{/if}
 					</button>
 				</form>
