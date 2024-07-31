f5227a65085426c0005c1086dc1a87a80b9d3ca8
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-07-31 18:45:43 +0000

    Revert "Set tools to default in migration"
    This reverts commit 87fdd094b73e2427af1a272e6697dafd4d466f1c.

--- src/lib/migrations/routines/07-reset-tools-in-settings.ts
+++ src/lib/migrations/routines/07-reset-tools-in-settings.ts
@@ -1,19 +1,14 @@
 import type { Migration } from ".";
 import { collections } from "$lib/server/database";
 import { ObjectId } from "mongodb";
-import { toolFromConfigs } from "$lib/server/tools";
 
 const resetTools: Migration = {
 	_id: new ObjectId("000000000007"),
-	name: "Set tools to default",
+	name: "Reset tools to empty",
 	up: async () => {
 		const { settings } = collections;
 
-		const defaultToolIds = toolFromConfigs
-			.filter((el) => el.isOnByDefault && !el.isHidden)
-			.map((el) => el._id.toString());
-
-		await settings.updateMany({}, { $set: { tools: defaultToolIds } });
+		await settings.updateMany({}, { $set: { tools: [] } });
 
 		return true;
 	},
