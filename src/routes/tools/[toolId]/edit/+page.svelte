eb4ef833ab6985b7af5ca70a76f67772814b5938
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-08-08 21:16:04 +0000

    fix return

--- src/routes/tools/[toolId]/edit/+page.svelte
+++ src/routes/tools/[toolId]/edit/+page.svelte
@@ -1,29 +1,21 @@
 <script lang="ts">
-	import { base } from "$app/paths";
-	import { afterNavigate, goto } from "$app/navigation";
 	import Modal from "$lib/components/Modal.svelte";
 	import ToolEdit from "../../ToolEdit.svelte";
 
-	let previousPage: string = base;
-
 	export let data;
 	export let form;
-
-	afterNavigate(({ from }) => {
-		if (!from?.url.pathname.includes("tools/")) {
-			previousPage = from?.url.toString() || previousPage;
-		}
-	});
 </script>
 
 <Modal
-	on:close={() => goto(previousPage)}
+	on:close={() => window.history.back()}
 	width="h-[95dvh] w-[90dvw] overflow-hidden rounded-2xl bg-white shadow-2xl outline-none sm:h-[85dvh] xl:w-[1200px] 2xl:h-[75dvh]"
 >
 	<ToolEdit
 		bind:form
 		tool={data.tool}
 		readonly={!data.tool.createdByMe}
-		on:close={() => goto(previousPage)}
+		on:close={() => {
+			window.history.back();
+		}}
 	/>
 </Modal>
--- src/routes/tools/new/+page.svelte
+++ src/routes/tools/new/+page.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
-	import { base } from "$app/paths";
-	import { afterNavigate, goto } from "$app/navigation";
 	import Modal from "$lib/components/Modal.svelte";
 	import ToolEdit from "../ToolEdit.svelte";
 
-	let previousPage: string = base;
-
 	export let form;
-
-	afterNavigate(({ from }) => {
-		previousPage = from?.url.toString() || previousPage;
-	});
 </script>
 
 <Modal
-	on:close={() => goto(previousPage)}
+	on:close={() => window.history.back()}
 	width="h-[95dvh] w-[90dvw] overflow-hidden rounded-2xl bg-white shadow-2xl outline-none sm:h-[85dvh] xl:w-[1200px] 2xl:h-[75dvh]"
 >
-	<ToolEdit bind:form on:close={() => goto(previousPage)} />
+	<ToolEdit bind:form on:close={() => window.history.back()} />
 </Modal>
