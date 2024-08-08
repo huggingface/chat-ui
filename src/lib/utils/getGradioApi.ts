fc217490c18daa7eb7221fe155178324716b444e
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-08-08 12:14:18 +0000

    fix types with gradio update

--- src/lib/utils/getGradioApi.ts
+++ src/lib/utils/getGradioApi.ts
@@ -1,9 +1,11 @@
 import { base } from "$app/paths";
-import type { ApiInfo, JsApiData } from "@gradio/client/dist/types";
+import type { Client } from "@gradio/client";
+
+export type ApiReturnType = Awaited<ReturnType<typeof Client.prototype.view_api>>;
 
 export async function getGradioApi(space: string) {
-	const api: ApiInfo<JsApiData> = await fetch(`${base}/api/spaces-config?space=${space}`).then(
-		(res) => res.json()
+	const api: ApiReturnType = await fetch(`${base}/api/spaces-config?space=${space}`).then((res) =>
+		res.json()
 	);
 	return api;
 }
