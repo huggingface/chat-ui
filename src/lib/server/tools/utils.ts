5550b13537df3fb664cd1c7e8e249a1bfd0a5f02
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-08-14 19:46:49 +0000

    fix(tools): remove debug log

--- src/lib/server/tools/utils.ts
+++ src/lib/server/tools/utils.ts
@@ -34,7 +34,6 @@ export async function* callSpace<TInput extends unknown[], TOutput extends unkno
 
 	let data;
 	for await (const output of job) {
-		console.log({ output });
 		if (output.type === "data") {
 			data = output.data as TOutput;
 		}
