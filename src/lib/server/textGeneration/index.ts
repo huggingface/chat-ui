b3bd1292f591b4756406861e18320c3b3e07bb7d
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-09-23 12:34:59 +0200

    fix(websearch): fix `webSearch` flag use for model with tool use (#1480)

--- src/lib/server/textGeneration/index.ts
+++ src/lib/server/textGeneration/index.ts
@@ -62,10 +62,7 @@ async function* textGenerationWithoutTitle(
 	// - it's not continuing a previous message
 	// - AND the model doesn't support tools and websearch is selected
 	// - OR the assistant has websearch enabled (no tools for assistants for now)
-	if (
-		!isContinue &&
-		((!model.tools && webSearch && !conv.assistantId) || assistantHasWebSearch(assistant))
-	) {
+	if (!isContinue && ((webSearch && !conv.assistantId) || assistantHasWebSearch(assistant))) {
 		webSearchResult = yield* runWebSearch(conv, messages, assistant?.rag);
 	}
 
