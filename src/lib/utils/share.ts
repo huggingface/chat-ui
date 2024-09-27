c3cde3474f7b65d214ad064664a193fc735ab164
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-09-27 12:14:09 +0000

    fix(front): fix clipboard copy on chrome

--- src/lib/utils/share.ts
+++ src/lib/utils/share.ts
@@ -17,10 +17,12 @@ export async function share(url: string, title: string, appendLeafId: boolean = 
 	if (navigator.share && !isDesktop(window)) {
 		navigator.share({ url, title });
 	} else {
-		if (document.hasFocus()) {
-			await navigator.clipboard.writeText(url);
-		} else {
-			alert("Document is not focused. Please try again.");
-		}
+		// this is really ugly
+		// but on chrome the clipboard write doesn't work if the window isn't focused
+		// and after we use confirm() to ask the user if they want to share, the window is no longer focused
+		// for a few ms until the confirm dialog closes. tried await tick(), tried window.focus(), didnt work
+		// bug doesnt occur in firefox, if you can find a better fix for it please do
+		await new Promise((resolve) => setTimeout(resolve, 250));
+		await navigator.clipboard.writeText(url);
 	}
 }
