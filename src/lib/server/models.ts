5a0c83731e369759f03bf425069a3502e0e321ec
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-10-25 07:34:14 +0000

    fix: use custom tool template only for huggingchat

--- src/lib/server/models.ts
+++ src/lib/server/models.ts
@@ -130,7 +130,7 @@ async function getChatPromptRender(
 			// or use the `rag` mode without the citations
 			const id = m.id ?? m.name;
 
-			if (id.startsWith("CohereForAI")) {
+			if (isHuggingChat && id.startsWith("CohereForAI")) {
 				formattedMessages = [
 					{
 						role: "system",
@@ -157,7 +157,7 @@ async function getChatPromptRender(
 					},
 					...formattedMessages,
 				];
-			} else if (id.startsWith("meta-llama")) {
+			} else if (isHuggingChat && id.startsWith("meta-llama")) {
 				const results = toolResults.flatMap((result) => {
 					if (result.status === ToolResultStatus.Error) {
 						return [
