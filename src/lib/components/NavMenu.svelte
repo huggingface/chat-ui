4a8a409f82cd32e5f36e572f5278893818514b4c
Ankush Rana <ankush.rana@bsc.es>
2024-10-29 14:28:31 +0100

    enable privacy

--- src/lib/components/NavMenu.svelte
+++ src/lib/components/NavMenu.svelte
@@ -171,11 +171,11 @@
 		Settings
 	</a>
 	<!-- {#if envPublic.PUBLIC_APP_NAME === "HuggingChat"} -->
-	<a
-		href="{base}/privacy"
-		class="flex h-9 flex-none items-center gap-1.5 rounded-lg pl-2.5 pr-2 text-gray-500 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700"
-	>
-		About & Privacy
-	</a>
+		<a
+			href="{base}/privacy"
+			class="flex h-9 flex-none items-center gap-1.5 rounded-lg pl-2.5 pr-2 text-gray-500 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700"
+		>
+			About & Privacy
+		</a>
 	<!-- {/if} -->
 </div>
