c9703f4d37539df38a707d26e709b5060a7ed69c
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-11-05 15:41:01 +0000

    fix: better websearch quotes

--- src/lib/components/chat/ChatMessage.svelte
+++ src/lib/components/chat/ChatMessage.svelte
@@ -47,6 +47,7 @@
 			const indices: number[] = (match.match(/\d+/g) || []).map(Number);
 			const links: string = indices
 				.map((index: number) => {
+					if (index === 0) return " ";
 					const source = webSearchSources[index - 1];
 					if (source) {
 						return `<a href="${source.link}" target="_blank" rel="noreferrer" style="${linkStyle}">${index}</a>`;
