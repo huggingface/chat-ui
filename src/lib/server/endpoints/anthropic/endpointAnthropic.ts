84aac49d4c7bb4a1d5c8c9409c588d695e6aef60
evalstate <1936278+evalstate@users.noreply.github.com>
2024-11-18 16:17:18 +0000

    upstream merge, remove commented out console log line

--- src/lib/server/endpoints/anthropic/endpointAnthropic.ts
+++ src/lib/server/endpoints/anthropic/endpointAnthropic.ts
@@ -44,7 +44,6 @@ export async function endpointAnthropic(
 		throw new Error("Failed to import @anthropic-ai/sdk", { cause: e });
 	}
 
-	//	console.log("---->" + JSON.stringify(mime));
 	const anthropic = new Anthropic({
 		apiKey,
 		baseURL,
