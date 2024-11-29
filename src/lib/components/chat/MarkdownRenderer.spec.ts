db8c1fba60d3dc4fad6f17e938964119c8034a1a
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-11-29 09:08:00 +0000

    test: add more tests for markdown renderer

--- src/lib/components/chat/MarkdownRenderer.spec.ts
+++ src/lib/components/chat/MarkdownRenderer.spec.ts
@@ -44,29 +44,64 @@ describe("MarkdownRenderer", () => {
 		expect(link).toHaveAttribute("target", "_blank");
 		expect(link).toHaveAttribute("rel", "noreferrer");
 	});
-	it("handles groups of citations", () => {
+	it("handles groups of sources", () => {
 		render(MarkdownRenderer, {
 			content: "Hello there [1], [2], [3]",
 			sources: [
 				{
 					title: "foo",
-					link: "https://example.com",
+					link: "https://foo.com",
 				},
 				{
 					title: "bar",
-					link: "https://example.com",
+					link: "https://bar.com",
 				},
 				{
 					title: "baz",
-					link: "https://example.com",
+					link: "https://baz.com",
 				},
 			],
 		});
 		expect(screen.getAllByRole("link")).toHaveLength(3);
+		expect(screen.getAllByRole("link")[0]).toHaveAttribute("href", "https://foo.com");
+		expect(screen.getAllByRole("link")[1]).toHaveAttribute("href", "https://bar.com");
+		expect(screen.getAllByRole("link")[2]).toHaveAttribute("href", "https://baz.com");
+	});
+	it("does not render sources in code blocks", () => {
+		render(MarkdownRenderer, {
+			content: "```\narray[1]\n```",
+			sources: [
+				{
+					title: "foo",
+					link: "https://example.com",
+				},
+			],
+		});
+		expect(screen.queryByRole("link")).not.toBeInTheDocument();
 	});
 	it("doesnt render raw html directly", () => {
 		render(MarkdownRenderer, { content: "<button>Click me</button>" });
 		expect(screen.queryByRole("button")).not.toBeInTheDocument();
 		expect(screen.queryByRole("paragraph")).toHaveTextContent("<button>Click me</button>");
+	});
+	it("renders latex", () => {
+		const { baseElement } = render(MarkdownRenderer, { content: "$(oo)^2$" });
+		expect(baseElement.querySelectorAll(".katex")).toHaveLength(1);
+	});
+	it("does not render latex in code blocks", () => {
+		const { baseElement } = render(MarkdownRenderer, { content: "```\n$(oo)^2$\n```" });
+		expect(baseElement.querySelectorAll(".katex")).toHaveLength(0);
+	});
+	it("does not render latex in inline codes", () => {
+		const { baseElement } = render(MarkdownRenderer, { content: "`$oo` and `$bar`" });
+		expect(baseElement.querySelectorAll(".katex")).toHaveLength(0);
+	});
+	it("does not render latex across multiple lines", () => {
+		const { baseElement } = render(MarkdownRenderer, { content: "* $oo \n* $aa" });
+		expect(baseElement.querySelectorAll(".katex")).toHaveLength(0);
+	});
+	it("renders latex with some < and > symbols", () => {
+		const { baseElement } = render(MarkdownRenderer, { content: "$foo < bar > baz$" });
+		expect(baseElement.querySelectorAll(".katex")).toHaveLength(1);
 	});
 });
