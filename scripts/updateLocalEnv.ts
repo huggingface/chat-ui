b213f0563ca8230e9f21f0eaee2da368b100a5fc
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2025-02-13 14:17:38 +0100

    fix(hchat): update internal endpoint (#1716)

--- scripts/updateLocalEnv.ts
+++ scripts/updateLocalEnv.ts
@@ -26,7 +26,7 @@ let full_config = `${PUBLIC_CONFIG}\n${SECRET_CONFIG}`;
 
 // replace the internal proxy url with the public endpoint
 full_config = full_config.replaceAll(
-	"https://proxy.serverless.api-inference.huggingface.tech",
+	"https://internal.api-inference.huggingface.co",
 	"https://api-inference.huggingface.co"
 );
 
