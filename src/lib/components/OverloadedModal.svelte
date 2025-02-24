f018b394978dd4dd26c719781c3cc675abc95438
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2025-02-24 16:30:36 +0000

    fix: modal max width

--- src/lib/components/OverloadedModal.svelte
+++ src/lib/components/OverloadedModal.svelte
@@ -8,7 +8,7 @@
 	let { onClose }: { onClose: () => void } = $props();
 </script>
 
-<Modal on:close={onClose} width="max-w-[400px]">
+<Modal on:close={onClose} width="!max-w-[400px]">
 	<div
 		class="from-primary-500/40 via-primary-500/10 to-primary-500/0 flex w-full flex-col items-center gap-3 bg-gradient-to-b px-5 pb-4 pt-9 text-center sm:px-6"
 	>
