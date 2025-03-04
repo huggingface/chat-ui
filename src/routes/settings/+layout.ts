8b3ab353fb0cf01570ab8c911aee5b86f991f190
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2025-03-04 13:14:14 +0000

    refactor(settings): remove waterfall loading

--- src/routes/settings/+layout.ts
+++ src/routes/settings/+layout.ts
@@ -3,14 +3,12 @@ import { base } from "$app/paths";
 import type { Serialize } from "$lib/utils/serialize";
 
 export const load = async ({ parent }) => {
-	const { assistants } = await parent();
-
 	const reports = await fetch(`${base}/api/v2/user/reports`)
 		.then((res) => res.json() as Promise<Serialize<Report>[]>)
 		.catch(() => []);
 
 	return {
-		assistants: (await assistants).map((el) => ({
+		assistants: (await parent().then((data) => data.assistants)).map((el) => ({
 			...el,
 			reported: reports.some((r) => r.contentId === el._id && r.object === "assistant"),
 		})),
