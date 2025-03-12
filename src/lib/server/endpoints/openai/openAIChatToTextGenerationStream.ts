301ac8dbbee710ade742909af9f217ae28878295
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2025-03-12 16:03:34 +0100

    fix(endpoints): fix for tool calling on hf inference with openai endpoint type (#1754)
    * fix(endpoints): fix for tool calling on hf inference with openai endpoint type
    
    * moar fix
    
    * fix: typechecks

--- src/lib/server/endpoints/openai/openAIChatToTextGenerationStream.ts
+++ src/lib/server/endpoints/openai/openAIChatToTextGenerationStream.ts
@@ -49,10 +49,47 @@ export async function* openAIChatToTextGenerationStream(
 	let generatedText = "";
 	let tokenId = 0;
 	const toolCalls: ToolCallWithParameters[] = [];
+	let toolBuffer = ""; // XXX: hack because tools seem broken on tgi openai endpoints?
+
 	for await (const completion of completionStream) {
 		const { choices } = completion;
 		const content = choices[0]?.delta?.content ?? "";
 		const last = choices[0]?.finish_reason === "stop" || choices[0]?.finish_reason === "length";
+
+		// if the last token is a stop and the tool buffer is not empty, yield it as a generated_text
+		if (choices[0]?.finish_reason === "stop" && toolBuffer.length > 0) {
+			yield {
+				token: {
+					id: tokenId++,
+					special: true,
+					logprob: 0,
+					text: "",
+				},
+				generated_text: toolBuffer,
+				details: null,
+			} as TextGenerationStreamOutput;
+			break;
+		}
+
+		// weird bug where the parameters are streamed in like this
+		if (choices[0]?.delta?.tool_calls) {
+			const calls = Array.isArray(choices[0].delta.tool_calls)
+				? choices[0].delta.tool_calls
+				: [choices[0].delta.tool_calls];
+
+			if (
+				calls.length === 1 &&
+				calls[0].index === 0 &&
+				calls[0].id === "" &&
+				calls[0].type === "function" &&
+				!!calls[0].function &&
+				calls[0].function.name === null
+			) {
+				toolBuffer += calls[0].function.arguments;
+				continue;
+			}
+		}
+
 		if (content) {
 			generatedText = generatedText + content;
 		}
