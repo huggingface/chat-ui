fcec021e15c3682e97ae4ee83f1c492e2b400404
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2025-04-28 13:22:34 +0000

    feat: rework assistant intro UI

--- src/lib/components/chat/AssistantIntroduction.svelte
+++ src/lib/components/chat/AssistantIntroduction.svelte
@@ -58,138 +58,145 @@
 	const settings = useSettingsStore();
 </script>
 
-<div class="flex h-full w-full flex-col content-center items-center justify-center pb-52">
-	<div
-		class="relative mt-auto rounded-2xl bg-gray-100 text-gray-600 dark:border-gray-800 dark:bg-gray-800/60 dark:text-gray-300"
-	>
-		<div
-			class="mt-3 flex min-w-[80dvw] items-center gap-4 p-4 pr-1 sm:min-w-[440px] md:p-8 xl:gap-8"
-		>
-			{#if assistant.avatar}
-				<img
-					src={`${base}/settings/assistants/${assistant._id.toString()}/avatar.jpg?hash=${
-						assistant.avatar
-					}`}
-					alt="avatar"
-					class="size-16 flex-none rounded-full object-cover max-sm:self-start md:size-32"
-				/>
-			{:else}
-				<div
-					class="flex size-12 flex-none items-center justify-center rounded-full bg-gray-300 object-cover text-xl font-bold uppercase text-gray-500 dark:bg-gray-600 max-sm:self-start sm:text-4xl md:size-32"
-				>
-					{assistant?.name[0]}
+<div class="my-auto grid gap-8 lg:grid-cols-3">
+	<div class="lg:col-span-1">
+		<div>
+			<div class="mb-3 flex items-center">
+				{#if assistant.avatar}
+					<img
+						src={`${base}/settings/assistants/${assistant._id.toString()}/avatar.jpg?hash=${
+							assistant.avatar
+						}`}
+						alt="avatar"
+						class="mr-3 size-10 flex-none rounded-full object-cover"
+					/>
+				{:else}
+					<div
+						class="mr-3 flex size-10 flex-none items-center justify-center rounded-full bg-gray-300 object-cover text-xl font-bold uppercase text-gray-500 dark:bg-gray-600"
+					>
+						{assistant?.name[0]}
+					</div>
+				{/if}
+				<div class="text-2xl font-semibold">
+					{assistant.name}
 				</div>
-			{/if}
-
-			<div class="flex h-full flex-col gap-2 text-balance">
-				<p class="-mb-1">Assistant</p>
-
-				<p class="text-xl font-bold sm:text-2xl">{assistant.name}</p>
-				{#if assistant.description}
-					<p class="line-clamp-6 text-sm text-gray-500 dark:text-gray-400">
-						{assistant.description}
-					</p>
-				{/if}
-
-				{#if assistant?.tools?.length}
-					<div
-						class="flex h-5 w-fit items-center gap-1 rounded-full bg-purple-500/10 pl-1 pr-2 text-xs"
-						title="This assistant uses the websearch."
-					>
-						<CarbonTools class="text-sm text-purple-600" />
-						Has tools
-					</div>
-				{/if}
-				{#if hasRag}
-					<div
-						class="flex h-5 w-fit items-center gap-1 rounded-full bg-blue-500/10 pl-1 pr-2 text-xs"
-						title="This assistant uses the websearch."
-					>
-						<IconInternet classNames="text-sm text-blue-600" />
-						Has internet access
-					</div>
-				{/if}
-
-				{#if assistant.createdByName}
-					<div class="pt-1 text-sm text-gray-400 dark:text-gray-500">
-						Created by
-						<a class="hover:underline" href="{base}/assistants?user={assistant.createdByName}">
-							{assistant.createdByName}
-						</a>
-						{#if assistant.userCount && assistant.userCount > 1}
-							<span class="mx-1">·</span>
-							<div
-								class="inline-flex items-baseline gap-1 text-sm text-gray-400 dark:text-gray-500"
-								title="Number of users"
-							>
-								<CarbonUserMultiple class="text-xxs" />{formatUserCount(assistant.userCount)} users
-							</div>
-						{/if}
-					</div>
-				{/if}
 			</div>
-		</div>
+			<p class="line-clamp-5 text-base text-gray-600 dark:text-gray-400">
+				{assistant.description || "No description provided."}
+			</p>
 
-		<div class="absolute right-3 top-3 md:right-4 md:top-4">
-			<div class="flex flex-row items-center gap-1">
-				<button
-					class="flex h-7 items-center gap-1.5 rounded-full border bg-white px-2.5 py-1 text-gray-800 shadow-sm hover:shadow-inner dark:border-gray-700 dark:bg-gray-700 dark:text-gray-300/90 dark:hover:bg-gray-800 max-sm:px-1.5 md:text-sm"
-					onclick={() => {
-						if (!isCopied) {
-							share(shareUrl, assistant.name);
-							isCopied = true;
-							setTimeout(() => {
-								isCopied = false;
-							}, 2000);
-						}
-					}}
-				>
-					{#if isCopied}
-						<CarbonCheckmark class="text-xxs text-green-600 max-sm:text-xs" />
-						<span class="text-green-600 max-sm:hidden"> Link copied </span>
-					{:else}
-						<CarbonExport class="text-xxs max-sm:text-xs" />
-						<span class="max-sm:hidden"> Share </span>
-					{/if}
-				</button>
-				<a
-					href="{base}/settings/assistants/{assistant._id.toString()}"
-					class="flex h-7 items-center gap-1.5 rounded-full border bg-white px-2.5 py-1 text-gray-800 shadow-sm hover:shadow-inner dark:border-gray-700 dark:bg-gray-700 dark:text-gray-300/90 dark:hover:bg-gray-800 md:text-sm"
-					><IconGear class="text-xxs" />Settings</a
-				>
-			</div>
+			<button
+				onclick={() => {
+					settings.instantSet({
+						activeModel: models[0].name,
+					});
+					goto(`${base}/`);
+				}}
+				class="mt-4 inline-flex w-fit items-center rounded-md border border-gray-200 bg-gray-50 px-2 py-1 text-xs text-gray-500 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700"
+			>
+				<CarbonRenew class="mr-1.5 text-xxs" /> Reset to default model
+			</button>
 		</div>
-		<button
-			onclick={() => {
-				settings.instantSet({
-					activeModel: models[0].name,
-				});
-				goto(`${base}/`);
-			}}
-			class="absolute -bottom-6 right-2 inline-flex items-center justify-center text-xs text-gray-600 underline hover:brightness-50 dark:text-gray-400 dark:hover:brightness-110"
-		>
-			<CarbonRenew class="mr-1.5 text-xxs" /> Reset to default model
-		</button>
 	</div>
-	{#if assistant.exampleInputs}
-		<div class="mx-auto mt-auto w-full gap-8 sm:-mb-8">
-			<div class="md:col-span-2 md:mt-6">
-				<div
-					class="grid grid-cols-1 gap-3 {assistant.exampleInputs.length > 1
-						? 'md:grid-cols-2'
-						: ''}"
-				>
-					{#each assistant.exampleInputs as example}
-						<button
-							type="button"
-							class="truncate whitespace-nowrap rounded-xl border bg-gray-50 px-3 py-2 text-left text-smd text-gray-600 hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700"
-							onclick={() => dispatch("message", example)}
+	<div class="lg:col-span-2 lg:pl-24">
+		<div class="overflow-hidden rounded-xl border dark:border-gray-800">
+			<div class="flex flex-wrap items-center justify-between gap-2 p-3">
+				<div class="flex flex-wrap items-center gap-2">
+					<div class="hidden text-sm font-medium text-gray-600 dark:text-gray-400 sm:block">
+						About this Assistant
+					</div>
+					{#if assistant.createdByName}
+						<span class="hidden text-gray-400 sm:block">•</span>
+						<a
+							class="text-sm text-gray-500 hover:underline"
+							href="{base}/assistants?user={assistant.createdByName}"
 						>
-							{example}
-						</button>
-					{/each}
+							{#if !import.meta.env.SSR && window.innerWidth < 640}By
+							{/if}{assistant.createdByName}
+						</a>
+					{/if}
 				</div>
+				<div class="flex gap-1 self-start">
+					<button
+						class="btn flex h-7 w-7 rounded-full bg-gray-100 p-1 text-xs hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-600"
+						onclick={() => {
+							if (!isCopied) {
+								share(shareUrl, assistant.name);
+								isCopied = true;
+								setTimeout(() => {
+									isCopied = false;
+								}, 2000);
+							}
+						}}
+						title="Share assistant"
+					>
+						{#if isCopied}
+							<CarbonCheckmark class="text-green-600" />
+						{:else}
+							<CarbonExport />
+						{/if}
+					</button>
+					<a
+						href="{base}/settings/assistants/{assistant._id.toString()}"
+						aria-label="Settings"
+						title="Settings"
+						class="btn flex h-7 w-7 rounded-full bg-gray-100 p-1 text-xs hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-600"
+						><IconGear /></a
+					>
+				</div>
+			</div>
+			<div class="grid gap-3 bg-gray-50 p-3 text-sm dark:bg-gray-800/70">
+				<div class="flex flex-wrap gap-2">
+					{#if hasRag}
+						<div
+							class="flex h-6 items-center gap-1 rounded-full bg-blue-500/10 pl-1.5 pr-2.5 text-xs"
+							title="This assistant uses web search"
+						>
+							<IconInternet classNames="text-sm text-blue-600" />
+							Internet access
+						</div>
+					{/if}
+
+					{#if assistant?.tools?.length}
+						<div
+							class="flex h-6 items-center gap-1 rounded-full bg-purple-500/10 pl-1.5 pr-2.5 text-xs"
+							title="This assistant can use tools"
+						>
+							<CarbonTools class="text-sm text-purple-600" />
+							Has tools
+						</div>
+					{/if}
+
+					{#if assistant.userCount && assistant.userCount > 1}
+						<div
+							class="flex h-6 items-center gap-1 rounded-full bg-gray-500/10 pl-1.5 pr-2.5 text-xs"
+							title="Number of users"
+						>
+							<CarbonUserMultiple class="text-sm text-gray-600 dark:text-gray-400" />
+							{formatUserCount(assistant.userCount)} users
+						</div>
+					{/if}
+				</div>
+			</div>
+		</div>
+	</div>
+	{#if assistant.exampleInputs && assistant.exampleInputs.length > 0}
+		<div class="lg:col-span-3 lg:mt-6">
+			<p class="mb-3 text-center text-gray-600 dark:text-gray-300 lg:text-left">Examples</p>
+			<div
+				class="flex max-h-60 gap-2 overflow-x-auto pb-2 text-center scrollbar-thin scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-700 lg:grid lg:grid-cols-3 lg:overflow-y-auto lg:text-left"
+			>
+				{#each assistant.exampleInputs as example}
+					<button
+						type="button"
+						class="flex-shrink-0 rounded-xl border bg-gray-50 px-2.5 py-2 text-sm text-gray-600 hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 sm:px-3 lg:w-full xl:px-3.5 xl:text-base"
+						onclick={() => dispatch("message", example)}
+					>
+						{example}
+					</button>
+				{/each}
 			</div>
 		</div>
 	{/if}
+	<div class="h-40 sm:h-24"></div>
 </div>
