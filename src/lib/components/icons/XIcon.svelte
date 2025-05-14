f0aae5049563780fd759a03b5fede8a2a43012c5
Hong An Tran <hongantran3804@gmail.com>
2025-05-14 10:54:20 -0700

    WIP - Finish the first version pop up search

---
+++ src/lib/components/icons/XIcon.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	interface Props {
+		classNames?: string;
+	}
+
+	let { classNames = "" }: Props = $props();
+</script>
+
+<svg
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	class={`icon-sidebar text-token-text-tertiary hover:text-token-text-primary ${classNames}`}
+	><path
+		fill-rule="evenodd"
+		clip-rule="evenodd"
+		d="M5.63603 5.63604C6.02656 5.24552 6.65972 5.24552 7.05025 5.63604L12 10.5858L16.9497 5.63604C17.3403 5.24552 17.9734 5.24552 18.364 5.63604C18.7545 6.02657 18.7545 6.65973 18.364 7.05025L13.4142 12L18.364 16.9497C18.7545 17.3403 18.7545 17.9734 18.364 18.364C17.9734 18.7545 17.3403 18.7545 16.9497 18.364L12 13.4142L7.05025 18.364C6.65972 18.7545 6.02656 18.7545 5.63603 18.364C5.24551 17.9734 5.24551 17.3403 5.63603 16.9497L10.5858 12L5.63603 7.05025C5.24551 6.65973 5.24551 6.02657 5.63603 5.63604Z"
+		fill="currentColor"
+	></path></svg
+>
