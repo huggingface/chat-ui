9a2ff4c85032dfc0f354aa3679a428e5d06ebf32
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2025-05-15 18:17:28 +0200

    refactor: improve rendering performance on low-end hardware (#1825)
    refactor: improve rendering perf. on low-end hardware
    
    - Reintroduced MarkdownWorker for asynchronous processing of markdown content.
    
    - Debounce message content updates dynamically based on render time

--- src/lib/components/chat/MarkdownRenderer.svelte
+++ src/lib/components/chat/MarkdownRenderer.svelte
@@ -1,40 +1,45 @@
 <script lang="ts">
 	import type { WebSearchSource } from "$lib/types/WebSearch";
 	import { processTokens, processTokensSync, type Token } from "$lib/utils/marked";
-	// import MarkdownWorker from "$lib/workers/markdownWorker?worker";
+	import MarkdownWorker from "$lib/workers/markdownWorker?worker";
 	import CodeBlock from "../CodeBlock.svelte";
-	// import type { IncomingMessage, OutgoingMessage } from "$lib/workers/markdownWorker";
+	import type { IncomingMessage, OutgoingMessage } from "$lib/workers/markdownWorker";
 	import { browser } from "$app/environment";
 
 	import DOMPurify from "isomorphic-dompurify";
+	import { onMount } from "svelte";
+	import { updateDebouncer } from "$lib/utils/updates";
 
 	interface Props {
 		content: string;
 		sources?: WebSearchSource[];
 	}
 
-	// const worker = browser && window.Worker ? new MarkdownWorker() : null;
+	let worker: Worker | null = null;
 
 	let { content, sources = [] }: Props = $props();
 
 	let tokens: Token[] = $state(processTokensSync(content, sources));
 
 	async function processContent(content: string, sources: WebSearchSource[]): Promise<Token[]> {
-		// if (worker) {
-		// 	return new Promise((resolve) => {
-		// 		worker.onmessage = (event: MessageEvent<OutgoingMessage>) => {
-		// 			if (event.data.type !== "processed") {
-		// 				throw new Error("Invalid message type");
-		// 			}
-		// 			resolve(event.data.tokens);
-		// 		};
-		// 		worker.postMessage(
-		// 			JSON.parse(JSON.stringify({ content, sources, type: "process" })) as IncomingMessage
-		// 		);
-		// 	});
-		// } else {
-		return processTokens(content, sources);
-		// }
+		if (worker) {
+			return new Promise((resolve) => {
+				if (!worker) {
+					throw new Error("Worker not initialized");
+				}
+				worker.onmessage = (event: MessageEvent<OutgoingMessage>) => {
+					if (event.data.type !== "processed") {
+						throw new Error("Invalid message type");
+					}
+					resolve(event.data.tokens);
+				};
+				worker.postMessage(
+					JSON.parse(JSON.stringify({ content, sources, type: "process" })) as IncomingMessage
+				);
+			});
+		} else {
+			return processTokens(content, sources);
+		}
 	}
 
 	$effect(() => {
@@ -42,25 +47,40 @@
 			tokens = processTokensSync(content, sources);
 		} else {
 			(async () => {
-				tokens = await processContent(content, sources);
+				updateDebouncer.startRender();
+				tokens = await processContent(content, sources).then(
+					async (tokens) =>
+						await Promise.all(
+							tokens.map(async (token) => {
+								if (token.type === "text") {
+									token.html = DOMPurify.sanitize(await token.html);
+								}
+								return token;
+							})
+						)
+				);
+
+				updateDebouncer.endRender();
 			})();
 		}
 	});
 
-	DOMPurify.addHook("afterSanitizeAttributes", (node) => {
-		if (node.tagName === "A") {
-			node.setAttribute("target", "_blank");
-			node.setAttribute("rel", "noreferrer");
-		}
+	onMount(() => {
+		worker = browser && window.Worker ? new MarkdownWorker() : null;
+
+		DOMPurify.addHook("afterSanitizeAttributes", (node) => {
+			if (node.tagName === "A") {
+				node.setAttribute("target", "_blank");
+				node.setAttribute("rel", "noreferrer");
+			}
+		});
 	});
 </script>
 
 {#each tokens as token}
 	{#if token.type === "text"}
-		{#await token.html then html}
-			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-			{@html DOMPurify.sanitize(html)}
-		{/await}
+		<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+		{@html token.html}
 	{:else if token.type === "code"}
 		<CodeBlock code={token.code} rawCode={token.rawCode} />
 	{/if}
