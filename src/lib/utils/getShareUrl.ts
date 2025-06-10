993741eef40eca8bc1e997f4fe91ca08545200a3
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2025-06-10 06:02:16 +0000

    fix: share feature

--- src/lib/utils/getShareUrl.ts
+++
@@ -1,9 +0,0 @@
-import { base } from "$app/paths";
-import { page } from "$app/state";
-
-export function getShareUrl(url: URL, shareId: string): string {
-	return `${
-		page.data.publicConfig.PUBLIC_SHARE_PREFIX ||
-		`${page.data.publicConfig.PUBLIC_ORIGIN || url.origin}${base}`
-	}/r/${shareId}`;
-}
