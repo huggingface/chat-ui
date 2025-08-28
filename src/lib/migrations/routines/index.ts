e4ec42fdbbfdaa924a9c4592db9b2cc85ff40088
Victor Muštar <victor.mustar@gmail.com>
2025-08-28 17:52:03 +0200

    ui(modal): ensure Escape closes all modals by listening on window and backdrop

--- src/lib/migrations/routines/index.ts
+++ src/lib/migrations/routines/index.ts
@@ -1,14 +1,10 @@
 import type { ObjectId } from "mongodb";
 
-import updateSearchAssistant from "./01-update-search-assistants";
-import updateAssistantsModels from "./02-update-assistants-models";
 import type { Database } from "$lib/server/database";
 import updateMessageUpdates from "./04-update-message-updates";
 import updateMessageFiles from "./05-update-message-files";
 import trimMessageUpdates from "./06-trim-message-updates";
-import updateFeaturedToReview from "./08-update-featured-to-review";
 import deleteEmptyConversations from "./09-delete-empty-conversations";
-import updateReportsAssistantId from "./10-update-reports-assistantid";
 
 export interface Migration {
 	_id: ObjectId;
@@ -21,12 +17,8 @@ export interface Migration {
 }
 
 export const migrations: Migration[] = [
-	updateSearchAssistant,
-	updateAssistantsModels,
 	updateMessageUpdates,
 	updateMessageFiles,
 	trimMessageUpdates,
-	updateFeaturedToReview,
 	deleteEmptyConversations,
-	updateReportsAssistantId,
 ];
