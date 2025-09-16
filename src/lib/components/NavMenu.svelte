add3599ffcef8bc8b8d2bcff418e36a21d27b5bf
Victor Muštar <victor.mustar@gmail.com>
2025-09-16 22:39:52 +0200

    Add keyboard shortcut title to New Chat link in NavMenu

--- src/lib/components/NavMenu.svelte
+++ src/lib/components/NavMenu.svelte
@@ -118,6 +118,7 @@
 		href={`${base}/`}
 		onclick={handleNewChatClick}
 		class="flex rounded-lg border bg-white px-2 py-0.5 text-center shadow-sm hover:shadow-none dark:border-gray-600 dark:bg-gray-700 sm:text-smd"
+		title="Ctrl/Cmd + Shift + O"
 	>
 		New Chat
 	</a>
