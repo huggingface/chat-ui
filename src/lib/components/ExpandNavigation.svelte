c8d7fe5ef4603a9bf03d94319a7c3d824893b030
Victor Muštar <victor.mustar@gmail.com>
2026-02-03 09:53:39 +0100

    Revert "Adding tool-tip and updating color for sidebar toggle button. (#2044)"
    This reverts commit 4ed954b41b65e96a72b73de5e480fc50bdd809ac.

--- src/lib/components/ExpandNavigation.svelte
+++ src/lib/components/ExpandNavigation.svelte
@@ -10,10 +10,9 @@
 
 <button
 	onclick={onClick}
-	title={isCollapsed ? "Expand sidebar" : "Collapse sidebar"}
 	class="{classNames} group flex h-16 w-6 flex-col items-center justify-center -space-y-1 outline-none *:h-3 *:w-1 *:rounded-full *:hover:bg-gray-300 dark:*:hover:bg-gray-600 max-md:hidden {!isCollapsed
-		? '*:bg-gray-400 dark:*:bg-gray-500'
-		: '*:bg-gray-400 dark:*:bg-gray-500'}"
+		? '*:bg-gray-200/70 dark:*:bg-gray-800'
+		: '*:bg-gray-200 dark:*:bg-gray-700'}"
 	name="sidebar-toggle"
 	aria-label="Toggle sidebar navigation"
 >
